01238d3a7b23c6d2afa68a61aea0eb7d6882d07c
pkiop <pkiop@naver.com>
2020-11-18 22:24:02 +0900

    [FE] Feat: Button Atom의 story 작성

--- fe/src/atoms/Button/index.stories.tsx
+++ fe/src/atoms/Button/index.stories.tsx
@@ -16,12 +16,71 @@ Pair.args = {
   title: 'hello',
 };
 
-export const HELLO = () => {
-  const subTitle = 'HELLO';
-  return <Button title={subTitle} />;
+const localLoginSize = {
+  width: 3.8,
+  height: 1.2,
+  fontSize: 0.6,
 };
 
-export const BYE = () => {
-  const subTitle = 'BYE';
-  return <Button title={subTitle} />;
+export const Login = () => {
+  const title = '로그인';
+  return (
+    <Button
+      title={title}
+      bgColor="#20c997"
+      color="#ffffff"
+      width={localLoginSize.width}
+      height={localLoginSize.height}
+      fontSize={localLoginSize.fontSize}
+    />
+  );
+};
+
+export const Signup = () => {
+  const title = '회원가입';
+  return (
+    <Button
+      title={title}
+      bgColor="#ffffff"
+      color="#20c997"
+      width={localLoginSize.width}
+      height={localLoginSize.height}
+      fontSize={localLoginSize.fontSize}
+      border="1px solid #20c997"
+    />
+  );
+};
+
+const oauthLoginSize = {
+  width: 7.1,
+  height: 2,
+  fontSize: 1,
+};
+
+export const NaverLogin = () => {
+  const title = 'Naver Login';
+  return (
+    <Button
+      title={title}
+      bgColor="#19ce60"
+      color="#ffffff"
+      width={oauthLoginSize.width}
+      height={oauthLoginSize.height}
+      fontSize={oauthLoginSize.fontSize}
+    />
+  );
+};
+
+export const GitHubLogin = () => {
+  const title = 'GitHub Login';
+  return (
+    <Button
+      title={title}
+      bgColor="#080909"
+      color="#ffffff"
+      width={oauthLoginSize.width}
+      height={oauthLoginSize.height}
+      fontSize={oauthLoginSize.fontSize}
+    />
+  );
 };
