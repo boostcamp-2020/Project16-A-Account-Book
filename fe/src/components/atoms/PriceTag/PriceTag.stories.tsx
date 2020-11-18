c1052721bd0201d0b9a18d12b1d0e37ae96c0a72
rolled-potatoes <keep4404@gmail.com>
2020-11-18 19:07:48 +0900

    [FE] Fix : PriceTag story prop 수정
    normal 을 small로 오기입.

--- fe/src/components/atoms/PriceTag/PriceTag.stories.tsx
+++ fe/src/components/atoms/PriceTag/PriceTag.stories.tsx
@@ -16,6 +16,9 @@ export const BigPriceTagComponent = (): React.ReactElement => {
   return <PriceTag value={10000000} bold={false} size="big" />;
 };
 export const NormalPriceTagComponent = (): React.ReactElement => {
+  return <PriceTag value={10000000} bold={false} size="normal" />;
+};
+export const smallPriceTagComponent = (): React.ReactElement => {
   return <PriceTag value={10000000} bold={false} size="small" />;
 };
 export const ColorPriceTagComponent = (): React.ReactElement => {
