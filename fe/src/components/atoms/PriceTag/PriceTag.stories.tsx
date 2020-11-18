d1ff476161cfbff7e415ac1e10af88eef6f0aa6c
rolled-potatoes <keep4404@gmail.com>
2020-11-18 22:33:24 +0900

    [FE] Fix : 오타 수정

--- fe/src/components/atoms/PriceTag/PriceTag.stories.tsx
+++ fe/src/components/atoms/PriceTag/PriceTag.stories.tsx
@@ -6,20 +6,20 @@ export default {
   component: PriceTag,
 };
 
-export const BlodPriceTagComponent = (): React.ReactElement => {
-  return <PriceTag value={10000000} bold size="normal" />;
+export const BoldPriceTagComponent = (): React.ReactElement => {
+  return <PriceTag value={10000000} bold size="normal" color="#000" />;
 };
 export const NotBlodPriceTagComponent = (): React.ReactElement => {
-  return <PriceTag value={10000000} bold={false} size="normal" />;
+  return <PriceTag value={10000000} bold={false} size="normal" color="#000" />;
 };
 export const BigPriceTagComponent = (): React.ReactElement => {
-  return <PriceTag value={10000000} bold={false} size="big" />;
+  return <PriceTag value={10000000} bold={false} size="big" color="#000" />;
 };
 export const NormalPriceTagComponent = (): React.ReactElement => {
-  return <PriceTag value={10000000} bold={false} size="normal" />;
+  return <PriceTag value={10000000} bold={false} size="normal" color="#000" />;
 };
 export const smallPriceTagComponent = (): React.ReactElement => {
-  return <PriceTag value={10000000} bold={false} size="small" />;
+  return <PriceTag value={10000000} bold={false} size="small" color="#000" />;
 };
 export const ColorPriceTagComponent = (): React.ReactElement => {
   return <PriceTag value={10000000} bold={false} size="normal" color="gray" />;
