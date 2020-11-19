ca22371bd056770f1d431fb69285a3cf66ba21ac
rolled-potatoes <keep4404@gmail.com>
2020-11-19 11:51:46 +0900

    [FE] Chore : eslint 룰 수정
    styled-component에 타입을 지정하는데 eslint 에러로 인한 룰 수정.

--- fe/.eslintrc.js
+++ fe/.eslintrc.js
@@ -25,8 +25,8 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/require-default-props': 0,
     'react/prop-types': 0,
+    'no-undef': 0,
     'react/jsx-filename-extension': [
       1,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
