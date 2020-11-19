a04866355e9c9740db3404ba54eda202f2ec3612
rolled-potatoes <keep4404@gmail.com>
2020-11-19 16:56:19 +0900

    [FE] Fix : 전역 스타일 설정 및 스토리 북 수정

--- fe/src/components/atoms/PriceTag/PriceTag.stories.tsx
+++ fe/src/components/atoms/PriceTag/PriceTag.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { withKnobs, select, number } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../styles/theme';
+
 import PriceTag from '.';
 
 export default {
@@ -9,8 +12,12 @@ export default {
 };
 export const priceTag = () => {
   const value = number('value', 1000000);
-  const size = select('size', ['normal', 'big', 'small'], 'normal');
+  const size = select('size', Object.keys(theme.fontSize), 'md');
   const bold = select('bold', [true, false], true);
-  const color = select('color', ['black', 'gray', '#f00'], 'black');
-  return <PriceTag value={value} size={size} bold={bold} color={color} />;
+  const color = select('color', Object.keys(theme.color), 'black');
+  return (
+    <ThemeProvider theme={theme}>
+      <PriceTag value={value} size={size} bold={bold} color={color} />
+    </ThemeProvider>
+  );
 };
--- fe/src/components/atoms/PriceTag/style.ts
+++ fe/src/components/atoms/PriceTag/style.ts
@@ -3,28 +3,19 @@ import styled from 'styled-components';
 export interface Prop {
   bold: boolean;
   size?: string;
-  color: string;
+  color?: string;
+  theme: {
+    [propName: string]: any;
+  };
 }
 
-const PropSize = {
-  Small: 'small',
-  Big: 'big',
-  Normal: 'normal',
-};
 const PriceContainer = styled.div<Prop>`
-  font-weight: ${(prop): string => (prop.bold ? '900' : 'normal')};
-  font-size: ${(prop): string => {
-    switch (prop.size) {
-      case PropSize.Big: {
-        return '1.5rem';
-      }
-      case PropSize.Small: {
-        return '0.7rem';
-      }
-      default:
-        return '1rem';
-    }
+  font-weight: ${({ bold }) => (bold ? '900' : 'normal')};
+  font-size: ${({ size, theme }: Prop) => {
+    if (size) return theme.fontSize[size];
+    return theme.fontSize.md;
   }};
-  color: ${(prop): string => prop.color};
+  color: ${({ color, theme }) =>
+    color ? theme.color[color] : theme.color.black};
 `;
 export default PriceContainer;
