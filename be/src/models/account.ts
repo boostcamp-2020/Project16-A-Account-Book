40187abb88570de65554d5c21bbd0bb05315c5f2
yejineee <21700431@handong.edu>
2020-11-21 19:05:25 +0900

    [BE] Feat: account schema 작성
    가계부 스키마는 해당 가계부가 갖고 있는 거래내역, 설정된 카테고리,
    거래 방법 스키마의 objectId를 배열로 갖고 있다.

---
+++ be/src/models/account.ts
@@ -0,0 +1,30 @@
+import { Schema, Types, model, Document } from 'mongoose';
+
+const AccountSchema = new Schema({
+  transactions: [
+    {
+      type: Types.ObjectId,
+      ref: 'transaction',
+    },
+  ],
+  categories: [
+    {
+      type: Types.ObjectId,
+      ref: 'category',
+    },
+  ],
+  methods: [
+    {
+      type: Types.ObjectId,
+      ref: 'method',
+    },
+  ],
+});
+
+export interface Account extends Document {
+  transactions?: [String];
+  categories?: [String];
+  methods?: [String];
+}
+
+export const AccountModel = model<Account>('account', AccountSchema);
