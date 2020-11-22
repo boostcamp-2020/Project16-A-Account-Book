dcd8acc487efcce38366f9fb003568fccab7c456
pkiop <pkiop@naver.com>
2020-11-22 21:18:07 +0900

    [FE] Feat: Button 안에 들어가는 것 children으로 받도록 수정

--- fe/src/components/atoms/Button/index.tsx
+++ fe/src/components/atoms/Button/index.tsx
@@ -3,16 +3,16 @@ import { Button } from './style';
 
 export interface Props {
   size?: string;
-  title: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactElement | string;
 }
 
-const App = ({ size, title, onClick, ...props }: Props) => {
+const App = ({ size, onClick, children, ...props }: Props) => {
   const onClickHandler = onClick ? useCallback(onClick!, []) : undefined;
 
   return (
     <Button size={size} onClick={onClickHandler} {...props}>
-      {title}
+      {children}
     </Button>
   );
 };
