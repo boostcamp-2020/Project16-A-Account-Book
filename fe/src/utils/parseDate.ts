50727c845bd99a50b0fbcbbd9056de4d715397d0
pkiop <pkiop@naver.com>
2020-11-23 17:33:58 +0900

    [FE] Feat: Date입력을 년,월,일,요일 을 선택해 파싱하는 util함수 작성
    - parseString에 원하는 출력을 입력하면 출력해줍니다.
    - ex) parseString="ym" -> 2020년 2월

---
+++ fe/src/utils/parseDate.ts
@@ -0,0 +1,28 @@
+import { parseIsolatedEntityName } from 'typescript';
+
+const parseDate = (date: Date, parseString: String) => {
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const weekday = date.getDay();
+
+  return parseString
+    .split('')
+    .reduce((acc, character) => {
+      switch (character) {
+        case 'y':
+          return `${acc + year}년 `;
+        case 'm':
+          return `${acc + month}월 `;
+        case 'd':
+          return `${acc + day}일 `;
+        case 'z':
+          return `${acc + week[weekday]}요일 `;
+        default:
+          return `잘못된 입력이 들어왔습니다.`;
+      }
+    }, '')
+    .slice(0, -1);
+};
+export default parseDate;
