8de9ee89fab15520d1b2d5c22aa13a34ae080872
pkiop <pkiop@naver.com>
2020-11-23 21:54:15 +0900

    [FE] Feat: AccountDate 컴포넌트 위한 스타일 작성

--- fe/src/components/organisms/AccountDate/style.ts
+++ fe/src/components/organisms/AccountDate/style.ts
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 import DateMoneyHeader from 'components/molecules/DateMoneyHeader';
-
-export interface ButtonStyleProps {
-  size?: string;
-}
+import Transaction from 'components/molecules/Transaction';
 
 export const Header = styled(DateMoneyHeader)`
   border-bottom: ${({ theme }) => `1px solid ${theme.color.lightBorder}`};
   padding-bottom: 0.3rem;
+`;
+
+export const AccountTransaction = styled(Transaction)`
+  margin-top: 0.5rem;
 `;
 
 export const AccountDate = styled.div``;
