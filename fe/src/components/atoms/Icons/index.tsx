48cf9335a1de7615c86f5c4467401ef45452dfcc
yejineee <yygeniee@gmail.com>
2020-11-24 01:52:15 +0900

    [FE] Fix : Icon Component에서 iconTypes 삭제
    iconTypes는 Icon을 불러오는 컴포넌트에서 사용하도록 지정한 것이었다.
    그러나 svg파일이 늘어남에 따라, iconTypes도 늘어나게 되고,
    불필요하게 지정해줘야 하는 불편함이 있었다.
    따라서 Icon을 불러오는 컴포넌트에서 icon props에 바로 svg파일을 넣어주는
    것으로 변경하였다.

--- fe/src/components/atoms/Icons/index.tsx
+++ fe/src/components/atoms/Icons/index.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import plusSvg from 'assets/svg/plus.svg';
-import githubSvg from 'assets/svg/github.svg';
-import naverSvg from 'assets/svg/naver.svg';
 import IconStyle from './style';
 
 export interface Props {
@@ -9,14 +6,8 @@ export interface Props {
   size?: string;
 }
 
-export const iconTypes = {
-  plus: plusSvg,
-  github: githubSvg,
-  naver: naverSvg,
-};
-
-const Icon = ({ icon, size = 'md' }: Props): React.ReactElement => {
-  return <IconStyle src={icon} alt="icon" size={size} />;
+const Icon = ({ icon, size = 'md', ...props }: Props): React.ReactElement => {
+  return <IconStyle src={icon} alt="icon" size={size} {...props} />;
 };
 
 export default Icon;
