54063c80efa9df18e41950316aa19473f1afcd82
pkiop <pkiop@naver.com>
2020-11-23 21:52:01 +0900

    [FE] Feat: Transaction 컴포넌트 생성
    - Transaction 하나를 보여주는 컴포넌트 생성

---
+++ fe/src/components/molecules/Transaction/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import doubleArrowIcon from 'assets/svg/doubleArrow.svg';
+import {
+  Transaction,
+  TransactionIcon,
+  Client,
+  Classification,
+  Price,
+  Wrap,
+  Wrap1,
+  Wrap2,
+} from './style';
+
+export type transaction = {
+  id: string;
+  icon?: string;
+  client: string;
+  classification: string;
+  method: string;
+  price: number;
+};
+
+export interface Props {
+  trans: transaction;
+}
+
+const App = ({ trans, ...props }: Props) => {
+  const classificationString = `${trans.classification} | ${trans.method}`;
+
+  return (
+    <Transaction {...props}>
+      <TransactionIcon
+        icon={trans.icon ? trans.icon : doubleArrowIcon}
+        size="sm"
+      />
+      <Wrap>
+        <Wrap1>
+          <Client>{trans.client}</Client>
+          <Wrap2>
+            <Classification>{classificationString}</Classification>
+          </Wrap2>
+        </Wrap1>
+        <Price value={trans.price} />
+      </Wrap>
+    </Transaction>
+  );
+};
+
+export default App;
