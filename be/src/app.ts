3094e4fd9c2da8a1c854b2f607faa8775cf506eb
pkiop <pkiop@naver.com>
2020-11-23 09:22:51 +0900

    [BE] Feat: Alias 사용 테스트

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,7 +1,9 @@
 import Koa from 'koa';
+import authRouter from '@Router/auth';
 
 const app = new Koa();
 
+app.use(authRouter.routes());
 app.use((ctx: Koa.Context) => {
   ctx.body = 'hello, Koa!';
 });
