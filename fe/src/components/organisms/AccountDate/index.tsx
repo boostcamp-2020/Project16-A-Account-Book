5b2ce8adcd210a8244367d135c4b278161b96724
pkiop <pkiop@naver.com>
2020-11-23 21:53:48 +0900

    [FE] Feat: AccountDate 컴포넌트 레이아웃 작성

--- fe/src/components/organisms/AccountDate/index.tsx
+++ fe/src/components/organisms/AccountDate/index.tsx
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { AccountDate, Header } from './style';
+import { transaction } from 'components/molecules/Transaction';
+import { AccountDate, Header, AccountTransaction } from './style';
 
 export interface Props {
   date: Date;
+  transactionList: transaction[];
 }
 
-const App = ({ date, ...props }: Props) => {
+const App = ({ date, transactionList, ...props }: Props) => {
   const [totalPayment, setTotalPayment] = useState(10000);
+
+  let totalPay = 0;
+  const transactionListComponent = transactionList.map((transEl) => {
+    totalPay += transEl.price;
+    return <AccountTransaction key={transEl.id} trans={transEl} />;
+  });
+
   useEffect(() => {
-    setTotalPayment(20000);
-  }, []);
+    setTotalPayment(totalPay);
+  }, [totalPayment]);
+
   return (
     <AccountDate {...props}>
       <Header date={date} totalPayment={totalPayment} />
+      {transactionListComponent}
     </AccountDate>
   );
 };
