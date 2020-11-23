359a08798ebd2555536b82eafffc713a4be67886
pkiop <pkiop@naver.com>
2020-11-23 21:54:49 +0900

    [FE] Feat: AccountDate 컴포넌트 테스트 위한 컴포넌트 작성

--- fe/src/components/organisms/AccountDate/index.stories.tsx
+++ fe/src/components/organisms/AccountDate/index.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
+import locksvg from 'assets/svg/lock.svg';
+import { transaction } from 'components/molecules/Transaction';
 import AccountDate from '.';
 
 export default {
@@ -13,9 +15,34 @@ export default {
 export const AccountDateSample = () => {
   const date = new Date();
 
+  const testTransactionList: transaction[] = [
+    {
+      id: 'dekvmczld',
+      icon: locksvg,
+      client: '시원한 육개장',
+      classification: '식사',
+      method: '네이버 페이',
+      price: 10000,
+    },
+    {
+      id: 'akjlwkndlkx',
+      client: '얼큰한 국밥',
+      classification: '식사',
+      method: '네이버 페이',
+      price: 6000,
+    },
+    {
+      id: 'ajabjfdlkx',
+      client: '달달한 하리보',
+      classification: '간식',
+      method: '현금',
+      price: 30000,
+    },
+  ];
+
   return (
     <ThemeProvider theme={theme}>
-      <AccountDate date={date} />
+      <AccountDate date={date} transactionList={testTransactionList} />
     </ThemeProvider>
   );
 };
