3ac65083bb317b5748ab4d36d63c03565572d0cd
yejineee <yygeniee@gmail.com>
2020-11-23 17:52:20 +0900

    [FE] Feat: icon component style 지정
    icon component에서 아이콘의 너비와 폭을 지정해줘야 한다.
    props로 'xs', 'sm', 'md', 'lg', 'xl'을 받으면
    theme에서 해당 key의 value로 설정한다.
    만약 위의 값이 아니라면, 직접 지정해준 사이즈로 width와 height를 설정한다.

---
+++ fe/src/components/atoms/Icons/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export interface Prop {
+  size: string;
+  theme: {
+    [propName: string]: any;
+  };
+}
+
+export default styled.img<Prop>`
+  width: ${({ size, theme }) => theme.len[size] || size};
+  height: ${({ size, theme }) => theme.len[size] || size};
+`;
