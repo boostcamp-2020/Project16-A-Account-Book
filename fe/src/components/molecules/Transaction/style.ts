7100c3dfeecd70d32721d635468c7378ddc8e120
pkiop <pkiop@naver.com>
2020-11-23 21:53:15 +0900

    [FE] Feat: Transaction 컴포넌트 구현 위한 스타일 파일 작성

---
+++ fe/src/components/molecules/Transaction/style.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import Icon from 'components/atoms/Icons';
+import PriceTag from 'components/atoms/PriceTag';
+
+export const Transaction = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const Wrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Wrap1 = styled.div`
+  margin-left: 0.5rem;
+`;
+
+export const Wrap2 = styled.div`
+  display: flex;
+`;
+
+export const TransactionIcon = styled(Icon)``;
+
+export const Client = styled.div``;
+
+export const Classification = styled.div`
+  color: ${({ theme }) => theme.color.darkgray};
+`;
+
+export const Price = styled(PriceTag)`
+  margin-left: 0.5rem;
+`;
+
+export default {};
