8c93807a846437a531b239b6a1b30d8c65207e61
pkiop <pkiop@naver.com>
2020-11-24 15:58:47 +0900

    [FE] Feat: knob 추가

--- fe/src/components/molecules/Transaction/index.stories.tsx
+++ fe/src/components/molecules/Transaction/index.stories.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import locksvg from 'assets/svg/lock.svg';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import Transaction, { transaction } from '.';
 
 export default {
-  title: 'molecules/AccountDate',
+  title: 'molecules/Transaction',
   component: Transaction,
+  decorators: [withKnobs],
 };
 
-export const TransactionSample1 = () => {
-  const testTransaction: transaction = {
+export const TransactionSample = () => {
+  const testTransaction = object<transaction>('transaction', {
     id: 'slakvken',
     client: '얼큰한 국밥',
     classification: '식사',
     method: '네이버 페이',
     price: 6000,
-  };
+  });
 
   return (
     <ThemeProvider theme={theme}>
