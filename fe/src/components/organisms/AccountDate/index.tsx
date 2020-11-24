4aff92902843ec195214174c8b987eb1f997ba65
pkiop <pkiop@naver.com>
2020-11-24 12:51:03 +0900

    [FE] Fix: dependency 수정

--- fe/src/components/organisms/AccountDate/index.tsx
+++ fe/src/components/organisms/AccountDate/index.tsx
@@ -32,7 +32,7 @@ const App = ({ date, transactionList, ...props }: Props) => {
   useEffect(() => {
     setTotalPayment(res.totalPrice);
     setTransactionListComponent(res.transList);
-  });
+  }, []);
 
   return (
     <AccountDate {...props}>
