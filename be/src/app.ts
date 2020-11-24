9ee670a556f80da10047fb28361bad359c4fbb02
pkiop <pkiop@naver.com>
2020-11-24 15:27:29 +0900

    [BE] Feat: koa body값 읽기 위한 bodyparser추가

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,11 +1,15 @@
 import Koa from 'koa';
 import authRouter from 'router/auth';
+import bodyParser from 'koa-bodyparser';
+import router from 'router/index';
 import { connect as dbConnect } from './models';
 
 const app = new Koa();
 dbConnect();
 
+app.use(bodyParser());
 app.use(authRouter.routes());
+app.use(router.routes());
 app.use((ctx: Koa.Context) => {
   ctx.body = 'hello, Koa!';
 });
