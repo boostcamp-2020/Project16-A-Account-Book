2941fc80534940ecf6d5eb9eaad6161fe002cebf
rolled-potatoes <keep4404@gmail.com>
2020-11-24 17:35:05 +0900

    [BE] Refactor : Promise all로 변경
    seed 생성 중 독립적으로 생성 할 수 있는 것은 Promise all 하여 비동기로 생성.

--- be/src/seeds/index.ts
+++ be/src/seeds/index.ts
@@ -1,9 +1,9 @@
 import * as categorySeed from './category';
 import * as methodSeed from './method';
+import * as UserSeed from './user';
 
 export const totalSeed = async () => {
-  await categorySeed.up();
-  await methodSeed.up();
+  return Promise.all([UserSeed.up(), methodSeed.up(), categorySeed.up()]);
 };
 
 export default {};
