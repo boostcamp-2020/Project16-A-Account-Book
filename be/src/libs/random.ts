37659ea5a4e02fd2083be7b9fe8f1a90ed9a301f
rolled-potatoes <keep4404@gmail.com>
2020-11-24 20:45:46 +0900

    [BE] Feat : 랜덤 날짜와 스트링 값 받는 함수 추가

--- be/src/libs/random.ts
+++ be/src/libs/random.ts
@@ -14,3 +14,22 @@ export const randomString = ({ length = 5 }: { length: number }): string => {
   };
   return Array(length).fill(0).reduce(reducer, '');
 };
+
+export const randomDate = (): Date => {
+  const baseDate = new Date('2020-01-01 00:00:00').getTime();
+  const yearToMillisecond = 3.154e10;
+  const randomMilliscond = randomNumber({ start: 0, end: yearToMillisecond });
+  return new Date(baseDate + randomMilliscond);
+};
+interface RandomLengthString {
+  minLength?: number;
+  maxLength?: number;
+}
+export const randomLengthString = ({
+  minLength = 0,
+  maxLength = 100,
+}: RandomLengthString): string => {
+  const length = randomNumber({ start: minLength, end: maxLength });
+  const result = randomString({ length });
+  return result;
+};
