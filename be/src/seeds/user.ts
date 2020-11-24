e2380aaa32fbe003bcda6936d947f2aeb3c59794
rolled-potatoes <keep4404@gmail.com>
2020-11-24 17:53:36 +0900

    [BE] Refactor : seed 반환 값 설정
    외부키를 가지는 seed를 생성할 때 사용하기 위해 반환 함.

--- be/src/seeds/user.ts
+++ be/src/seeds/user.ts
@@ -1,11 +1,11 @@
 import { randomNumber, randomString } from '../libs/random';
-import { UserModel } from '../models';
+import { UserModel, User } from '../models';
 
 interface DummyArray {
   id: string;
 }
 const SEED_LENGTH = 10;
-export const up = async () => {
+export const up = async (): Promise<User> => {
   const reducer = (dummyArray: [DummyArray]): [DummyArray] => {
     const randomLength = randomNumber({ start: 5, end: 15 });
     const randomId = randomString({ length: randomLength });
@@ -15,7 +15,8 @@ export const up = async () => {
     return dummyArray;
   };
   const dummies = Array(SEED_LENGTH).fill(0).reduceRight(reducer, []);
-  await UserModel.create(dummies);
+  const users = await UserModel.create(dummies);
+  return users;
 };
 
 export const down = async () => {
