472eca67743903e6f3c640d657e000a872e01cfd
rolled-potatoes <keep4404@gmail.com>
2020-11-24 14:49:46 +0900

    [FE] Feat : 드롭다운 화면 밖 눌렀을 때 닫히는 이벤트 작성

--- fe/src/components/molecules/DropdownHeader/index.tsx
+++ fe/src/components/molecules/DropdownHeader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import DropdownButton from './style';
 
 interface Props {
@@ -9,11 +9,28 @@ const arrowCharacter = '▾';
 
 const DropdownButtonHeader = ({ title = '', children }: Props) => {
   const [visible, setVisible] = useState(false);
+  const dropdownContainer = useRef<HTMLDivElement>(null);
+  const outsideClickHandler = (event: any) => {
+    if (!dropdownContainer.current) return;
+    if (
+      dropdownContainer &&
+      !dropdownContainer.current.contains(event.target)
+    ) {
+      setVisible(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('click', outsideClickHandler);
+    return () => {
+      document.removeEventListener('click', outsideClickHandler);
+    };
+  });
+
   const toggleVisible = () => {
     setVisible(!visible);
   };
   return (
-    <div>
+    <div ref={dropdownContainer}>
       <DropdownButton onClick={toggleVisible}>
         <div>{`${title} ${arrowCharacter}`}</div>
       </DropdownButton>
