35114ad61bf1a904aeb333bfc2f89610f516b580
yejineee <yygeniee@gmail.com>
2020-11-25 15:56:23 +0900

    [FE] Feat: LabelWrap 스타일 추가
    LabelWrap의 스타일을 추가하였다.
    레이아웃을 잡아주는 역할을 한다.

---
+++ fe/src/components/molecules/LabelWrap/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LabelWrap = styled.div`
+  display: flex;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex: 1 1 30%;
+  padding: 1rem;
+
+  align-items: center;
+`;
+
+export const Contents = styled.div`
+  flex: 2 1 70%;
+`;
