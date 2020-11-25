525583be29d1940edb9ee4f37b8f5b86893282de
pkiop <pkiop@naver.com>
2020-11-25 12:17:52 +0900

    [BE] Chore: koa-cors 추가
    * cors문제를 일단 해결하기 위해 설치했습니다.

--- be/src/app.ts
+++ be/src/app.ts
@@ -2,12 +2,14 @@ import Koa from 'koa';
 import authRouter from 'router/auth';
 import bodyParser from 'koa-bodyparser';
 import router from 'router/index';
+import cors from '@koa/cors';
 import { connect as dbConnect } from './models';
 
 const app = new Koa();
 dbConnect();
 
 app.use(bodyParser());
+app.use(cors());
 app.use(authRouter.routes());
 app.use(router.routes());
 app.use((ctx: Koa.Context) => {
