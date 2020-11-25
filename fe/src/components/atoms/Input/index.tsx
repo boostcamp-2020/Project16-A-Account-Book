f9bae32dab10c6f690832340d4a57061c8dc2e84
yejineee <yygeniee@gmail.com>
2020-11-25 15:49:58 +0900

    [FE] Feat: Input 컴포넌트에 onClick props 추가
    input type이 버튼일 때, 클릭 이벤트 핸들러를 추가해야 하므로,
    onClick props를 추가하였다.

--- fe/src/components/atoms/Input/index.tsx
+++ fe/src/components/atoms/Input/index.tsx
@@ -5,6 +5,7 @@ export interface Props {
   id?: string;
   name?: string;
   onChangeHandler?: any;
+  onClick?: any;
   disabled?: boolean;
   placeholder?: string;
   value?: any;
@@ -16,6 +17,7 @@ const Input = ({
   disabled,
   value,
   onChangeHandler,
+  onClick,
   type,
   name,
   id,
@@ -28,6 +30,7 @@ const Input = ({
       placeholder={placeholder}
       type={type}
       onChange={onChangeHandler}
+      onClick={onClick}
       value={value}
       disabled={disabled}
       {...props}
