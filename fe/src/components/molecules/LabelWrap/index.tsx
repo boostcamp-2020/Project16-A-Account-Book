d12d7dcacb1449b11f3519efa1855cb09ea68f69
yejineee <yygeniee@gmail.com>
2020-11-25 15:53:28 +0900

    [FE] Feat: LabelWrap component 추가
    폼을 만들 때, 라벨 + 인풋의 틀을 잡아주는 LabelWrap component를 추가하였다.
    Label 옆에 input외에도, select와 같은 태그들이 올 수 있기에, children을 받아서
    라벨 아래에 두었다.

---
+++ fe/src/components/molecules/LabelWrap/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Props as InputProps } from 'components/atoms/Input';
+import * as S from './style';
+
+export interface Props extends InputProps {
+  htmlFor: string;
+  title: string | React.ReactElement;
+  children: React.ReactElement | React.ReactElement[] | string;
+}
+
+const LabelWrap = ({ htmlFor, title, children }: Props): React.ReactElement => {
+  return (
+    <S.LabelWrap>
+      <S.Label htmlFor={htmlFor}>{title}</S.Label>
+      <S.Contents>{children}</S.Contents>
+    </S.LabelWrap>
+  );
+};
+
+export default LabelWrap;
