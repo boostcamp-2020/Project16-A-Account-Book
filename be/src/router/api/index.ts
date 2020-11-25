b738db573b77e7c887da5ca0f251188327f5c2e5
pkiop <pkiop@naver.com>
2020-11-25 15:47:09 +0900

    [FE/BE] Chore: 프로젝트 구조 개선 반영

--- be/src/router/api/index.ts
+++
@@ -1,8 +0,0 @@
-import Router from 'koa-router';
-import transactionRouter from 'router/api/transaction';
-
-const router = new Router();
-
-router.use('/transaction', transactionRouter.routes());
-
-export default router;
--- be/src/router/index.ts
+++
@@ -1,8 +0,0 @@
-import Router from 'koa-router';
-import apiRouter from 'router/api';
-
-const router = new Router();
-
-router.use('/api', apiRouter.routes());
-
-export default router;
