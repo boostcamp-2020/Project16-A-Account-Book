c59ca8fda696d42680a70cca986cb88883e20e38
pkiop <pkiop@naver.com>
2020-11-26 19:02:13 +0900

    [BE] Feat: GET/transaction에서 한달치 데이터 가져오도록 수정

--- be/src/services/transaction/index.ts
+++ be/src/services/transaction/index.ts
@@ -1,12 +1,63 @@
 import { TransactionModel, Transaction } from 'models/transaction';
+import getOneMonthRange from 'libs/date';
 
-export const getTransaction = async () => {
-  return TransactionModel.find().populate('category').populate('method');
+export interface TransactionDBType {
+  excludeFromBudget: boolean;
+  _id: string;
+  client: string;
+  date: Date;
+  memo?: string;
+  method: {
+    _id: string;
+    title: string;
+    __v?: number;
+  };
+  category: {
+    _id: string;
+    type: string;
+    title: string;
+    __v?: number;
+  };
+  price: number;
+}
+
+const oneMonthTransactionsReducer = (acc: any, transaction: Transaction) => {
+  const year = transaction.date.getFullYear();
+  const month = transaction.date.getMonth() + 1;
+  const date = transaction.date.getDate();
+  const key = `${year}-${month}-${date}`;
+  if (acc[key]) {
+    acc[key].push(transaction);
+    return {
+      ...acc,
+    };
+  }
+  acc[key] = [transaction];
+  return {
+    ...acc,
+  };
+};
+export const getTransaction = async ({
+  year,
+  month,
+}: {
+  year: string;
+  month: string;
+}) => {
+  const oneMonthTransactions: Transaction[] = await TransactionModel.find()
+    .populate('category')
+    .populate('method')
+    .where('date')
+    .gte(new Date(getOneMonthRange(year, month).start))
+    .lt(new Date(getOneMonthRange(year, month).end))
+    .sort('date');
+
+  const result = oneMonthTransactions.reduce(oneMonthTransactionsReducer, {});
+  return result;
 };
 
 export const createTransaction = async ({
   client,
-  classification,
   date,
   memo,
   method,
@@ -16,7 +67,6 @@ export const createTransaction = async ({
 }: Transaction) => {
   const newTransaction = new TransactionModel({
     client,
-    classification,
     date,
     memo,
     method,
