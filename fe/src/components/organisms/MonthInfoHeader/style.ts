c646e1044603df506a7f131d136dba2e4f1b02b8
pkiop <pkiop@naver.com>
2020-11-26 16:56:16 +0900

    [FE] Fix: 미처 수정하지 못했던 color 수정

--- fe/src/components/organisms/MonthInfoHeader/style.ts
+++ fe/src/components/organisms/MonthInfoHeader/style.ts
@@ -31,6 +31,6 @@ export const MonthInfoHeaderContainer = styled.section`
   min-height: 4rem;
   justify-content: center;
   align-items: center;
-  background: ${({ theme }) => theme.color.green};
+  background: ${({ theme }) => theme.color.brandColor};
 `;
 export default {};
