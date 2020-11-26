c2c93718d3e5930a21d3638e8b1d8b21ba8a9ad4
pkiop <pkiop@naver.com>
2020-11-26 13:53:54 +0900

    [BE] Fix: transaction 라우팅 transactions로 수정

--- be/src/routers/api/api/transaction/index.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import transactionController from 'controller/transaction';
-
-const router = new Router();
-
-router.get('/', transactionController.get);
-router.post('/', transactionController.post);
-
-export default router;
--- be/src/routers/api/index.ts
+++ be/src/routers/api/index.ts
@@ -1,8 +1,10 @@
 import Router from 'koa-router';
-import apiRouter from 'router/api';
+import transactionRouter from 'routers/api/transaction';
+import authRouter from 'routers/api/auth';
 
 const router = new Router();
 
-router.use('/api', apiRouter.routes());
+router.use('/transactions', transactionRouter.routes());
+router.use('/auth', authRouter.routes());
 
 export default router;
