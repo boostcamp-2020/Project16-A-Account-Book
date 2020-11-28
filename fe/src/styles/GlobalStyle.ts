b5a40399ea65b7ac105dfde04429515782aedf32
rolled-potatoes <keep4404@gmail.com>
2020-11-28 15:48:04 +0900

    [FE] : 글로벌 스타일 적용 안되는 버그 수정

--- fe/src/styles/GlobalStyle.ts
+++ fe/src/styles/GlobalStyle.ts
@@ -1,16 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'BMEULJIRO';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/BMEULJIRO.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
+const GlobalStyle = createGlobalStyle`    
+    @font-face {
+      font-family: 'Bmeuljiro';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/BMEULJIRO.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+    }
+    
+  body{
+    ${reset} ;
+    font-family: Bmeuljiro,sans-serif !important;
+    button{
+      font-family :  Bmeuljiro,sans-serif !important;
+    }
+    box-sizing: border-box;
   }
-
-  ${reset};
-  box-sizing: border-box;
 `;
 
 export default GlobalStyle;
