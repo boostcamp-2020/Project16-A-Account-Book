6fa366dc425b6d0a0aa73ec9834239cc8963ff6b
yejineee <yygeniee@gmail.com>
2020-12-01 00:56:55 +0900

    [FE] Chore: 카테고리별 정보 컨트롤러 라우팅
    /api/transactions/:accountObjId/categories 에 대한 라우팅을 하였고,
    그에 맞는 컨트롤러를 연결하였다.

--- be/src/routers/api/index.ts
+++ be/src/routers/api/index.ts
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 import transactionRouter from 'routers/api/transaction';
+import categoryRouter from 'routers/api/category';
 import authRouter from 'routers/api/auth';
 
 const router = new Router();
 
+router.use('/categories', categoryRouter.routes());
 router.use('/transactions', transactionRouter.routes());
 router.use('/auth', authRouter.routes());
 
