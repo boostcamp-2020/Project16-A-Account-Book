12facb0bbe721abe271231921683f3982c96f678
pkiop <pkiop@naver.com>
2020-12-01 15:36:35 +0900

    [FE] Feat: Date객체의 최대 날짜를 출력해주는 함수 구현

--- fe/src/utils/date.ts
+++ fe/src/utils/date.ts
@@ -3,8 +3,7 @@ import dayjs from 'dayjs';
 export default {
   dateFormatter: (date: Date): string => dayjs(date).format('YYYY-MM-DD'),
   monthMaxDate: (date: Date): number => {
-    const nextMonth = `${date.getFullYear()}-${date.getMonth() + 1}-{-1}`;
-    const nowMaxDate = new Date(nextMonth);
+    const nowMaxDate = new Date(date.getFullYear(), date.getMonth() + 1, -1);
     return nowMaxDate.getDate() + 1;
   },
 };
