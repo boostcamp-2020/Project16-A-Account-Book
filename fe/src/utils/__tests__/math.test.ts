154185480c8f7e1cc60108971cec12b1b5ee9ae4
yejineee <yygeniee@gmail.com>
2020-12-01 13:07:27 +0900

    [FE] Fix: 테스트 이름 수정 및 호출 함수 이름 수정
    유틸 함수를 compByKey에서 getCompFuncByKey로 수정함에 따라
    호출하는 함수 이름을 수정하였다.
    또한, 키 값에 해당하는 값들을 다 더하는지 확인하는 테스트케이스
    이름에 오류가 있어서, 이름을 수정한다.

--- fe/src/utils/__tests__/math.test.ts
+++ fe/src/utils/__tests__/math.test.ts
@@ -6,18 +6,18 @@ describe('객체의 배열이 주어졌을 때,', () => {
   const descAgeList = [{ age: 5 }, { age: 3 }, { age: 1 }];
 
   test('주어진 키 값에 맞춰서 오름차순으로 정렬한다', () => {
-    const ascAgeComp = math.compByKey('age');
+    const ascAgeComp = math.getCompFuncByKey('age');
     expect(ageList.sort(ascAgeComp)).toEqual(ascAgeList);
     expect(ageList.sort(ascAgeComp)).not.toEqual(descAgeList);
   });
   test('주어진 키 값에 맞춰서 내림차순으로 정렬한다', () => {
-    const descAgeComp = math.compByKey('age', false);
+    const descAgeComp = math.getCompFuncByKey('age', false);
     expect(ageList.sort(descAgeComp)).toEqual(descAgeList);
     expect(ageList.sort(descAgeComp)).not.toEqual(ascAgeList);
   });
 });
 
-test('주어진 키 값에 맞춰서 오름차순으로 정렬한다', () => {
+test('주어진 키에 해당하는 값들을 더한다.', () => {
   const ageList = [{ age: 5 }, { age: 1 }, { age: 3 }];
   expect(math.sumByKey(ageList, 'age')).toBe(9);
 });
