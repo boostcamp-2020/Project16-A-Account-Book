fb14950da809534b4fc55c48b0b3fc2913d598dd
pkiop <pkiop@naver.com>
2020-12-01 15:25:25 +0900

    [FE] Fix: monthMaxDate 추가

--- fe/src/utils/date.ts
+++ fe/src/utils/date.ts
@@ -2,4 +2,9 @@ import dayjs from 'dayjs';
 
 export default {
   dateFormatter: (date: Date): string => dayjs(date).format('YYYY-MM-DD'),
+  monthMaxDate: (date: Date): number => {
+    const nextMonth = `${date.getFullYear()}-${date.getMonth() + 1}-{-1}`;
+    const nowMaxDate = new Date(nextMonth);
+    return nowMaxDate.getDate() + 1;
+  },
 };
