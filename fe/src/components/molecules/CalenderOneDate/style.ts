746ef5de33c3433fee0f2a5592c1f1074fc6c232
pkiop <pkiop@naver.com>
2020-12-01 13:45:22 +0900

    [FE] Fix: 달력 한칸 고정된 높이 지정

--- fe/src/components/molecules/CalenderOneDate/style.ts
+++ fe/src/components/molecules/CalenderOneDate/style.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import Button from 'components/atoms/Button';
 
-export const CalenderOneDate = styled(Button)``;
+export const CalenderOneDate = styled(Button)`
+  height: 5rem;
+`;
 
 export const DateText = styled.div``;
 
