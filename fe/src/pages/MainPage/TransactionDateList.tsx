d49320ecb6345fc22187d1edfa132d283b3293fd
pkiop <pkiop@naver.com>
2020-12-01 20:25:01 +0900

    [FE] Refector: transaction store데이터로 totalprice만드는 함수 따로 store폴더에 저장

--- fe/src/pages/MainPage/TransactionDateList.tsx
+++ fe/src/pages/MainPage/TransactionDateList.tsx
@@ -18,46 +18,6 @@ const convertTransactionDBTypetoTransactionType = (
 
 type TransactionDBKeyValue = [date: string, transactions: any];
 
-const initTotalPrice = {
-  income: 0,
-  expense: 0,
-};
-
-const TransactionsReduce = (
-  oneDayPrice: { income: number; expense: number },
-  transaction: TransactionDBType,
-) => {
-  if (transaction.category.type === 'INCOME') {
-    return {
-      ...oneDayPrice,
-      income: oneDayPrice.income + transaction.price,
-    };
-  }
-  if (transaction.category.type === 'EXPENSE') {
-    return {
-      ...oneDayPrice,
-      expense: oneDayPrice.expense + transaction.price,
-    };
-  }
-  // TODO 이체 처리
-  return oneDayPrice;
-};
-
-export const calTotalPrices = (list: any) => {
-  return Object.values<TransactionDBType[]>(list).reduce(
-    (acc: { income: number; expense: number }, transactions) => {
-      const res = transactions.reduce(TransactionsReduce, {
-        ...initTotalPrice,
-      });
-      return {
-        income: acc.income + res.income,
-        expense: acc.expense + res.expense,
-      };
-    },
-    { ...initTotalPrice },
-  );
-};
-
 const TransactionDateList = ({ list }: { list: any }) => {
   const mapFunc = (item: TransactionDBKeyValue) => {
     const [date, transactions] = item;
--- fe/src/stores/Transaction/transactionStoreUtils.ts
+++ fe/src/stores/Transaction/transactionStoreUtils.ts
@@ -1,24 +1,6 @@
-import React from 'react';
-import AccountDate from 'components/organisms/AccountDate';
 import { TransactionDBType } from 'stores/Transaction';
 
-const convertTransactionDBTypetoTransactionType = (
-  input: TransactionDBType[],
-) => {
-  return input.map((el) => {
-    const { _id, category, method, ...other } = el;
-    return {
-      ...other,
-      id: _id,
-      category: category.title,
-      method: method.title,
-    };
-  });
-};
-
-type TransactionDBKeyValue = [date: string, transactions: any];
-
-const initTotalPrice = {
+export const initTotalPrice = {
   income: 0,
   expense: 0,
 };
@@ -57,22 +39,3 @@ export const calTotalPrices = (list: any) => {
     { ...initTotalPrice },
   );
 };
-
-const TransactionDateList = ({ list }: { list: any }) => {
-  const mapFunc = (item: TransactionDBKeyValue) => {
-    const [date, transactions] = item;
-    return (
-      <AccountDate
-        key={date}
-        date={new Date(date)}
-        transactionList={convertTransactionDBTypetoTransactionType(
-          transactions as [],
-        )}
-      />
-    );
-  };
-
-  return <>{Object.entries(list).map(mapFunc)}</>;
-};
-
-export default TransactionDateList;
