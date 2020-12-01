1bdb4c47dac771d550e9e1071b6c5aef37db8517
pkiop <pkiop@naver.com>
2020-12-01 16:32:34 +0900

    [FE] Feat: 한 달치 달력을 생성하는 기능 추가

--- fe/src/components/organisms/Calender/index.tsx
+++ fe/src/components/organisms/Calender/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import date from 'utils/date';
 import { TransactionDBType } from 'stores/Transaction';
 import * as S from './style';
 
@@ -20,6 +21,7 @@ const Calender = ({
   transactions,
   ...props
 }: Props): React.ReactElement => {
+  const defaultStartDay = isSundayStart ? 0 : 1;
   const { oneDateList } = Object.entries(transactions).reduce(
     (acc: accType, el: any) => {
       const [key, value]: [string, TransactionDBType[]] = el;
@@ -42,6 +44,7 @@ const Calender = ({
         },
       );
       return {
+        ...acc,
         oneDateList: [...acc.oneDateList, { ...res, date: key }],
       };
     },
@@ -50,13 +53,41 @@ const Calender = ({
     },
   );
 
-  new Array(new Date(new Date(oneDateList[0].date) - 1).getDay() + 1).fill(0);
-  console.log('oneDateList : ', oneDateList);
+  const nowDate = new Date(oneDateList[0].date);
+  const maxDate = date.monthMaxDate(nowDate);
+  const nowYear = nowDate.getFullYear();
+  const nowMonth = nowDate.getMonth();
+  const oneWeekListData = [];
+  let oneDateComponent = [];
+  let oneDateListIdx = 0;
+  for (let dateLoopIdx = 1; dateLoopIdx <= maxDate; dateLoopIdx += 1) {
+    const nowElement = oneDateList[oneDateListIdx];
+    if (nowElement) {
+      const nowElementDate = nowElement.date.split('-')[2];
+      if (nowElementDate === String(dateLoopIdx)) {
+        oneDateComponent.push({ ...nowElement, date: dateLoopIdx });
+        oneDateListIdx += 1;
+      }
+    } else {
+      oneDateComponent.push({ date: dateLoopIdx });
+    }
+    if (new Date(nowYear, nowMonth, dateLoopIdx).getDay() === defaultStartDay) {
+      oneWeekListData.push(oneDateComponent);
+      oneDateComponent = [];
+    }
+  }
+  if (oneWeekListData.length !== 0) {
+    oneWeekListData.push(oneDateComponent);
+  }
+
+  const oneWeekComponentList = oneWeekListData.map((el) => {
+    return <S.OneWeek key={el[0].date} oneDateList={el} />;
+  });
 
   return (
     <S.Calender {...props}>
       <S.DayBar isSundayStart={isSundayStart} />
-      <div>test</div>
+      {oneWeekComponentList}
     </S.Calender>
   );
 };
