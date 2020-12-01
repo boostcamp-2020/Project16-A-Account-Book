1f31e58fc02c58099e881e5506161c79b96ee997
pkiop <pkiop@naver.com>
2020-12-01 15:25:07 +0900

    [FE] Fix: default width추가

--- fe/src/components/molecules/CalenderOneWeek/style.ts
+++ fe/src/components/molecules/CalenderOneWeek/style.ts
@@ -4,10 +4,13 @@ import PriceTagComponent from 'components/atoms/PriceTag';
 import CalenderOneDate from 'components/molecules/CalenderOneDate';
 
 export const CalenderOneWeek = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
 `;
-export const OneDate = styled(CalenderOneDate)``;
+export const OneDate = styled(CalenderOneDate)`
+  width: 70%;
+`;
 export const DateWrap = styled.div`
   display: flex;
 `;
