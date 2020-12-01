d8116d3c16ad95b12654622056cc4eb3682ea74f
dbstjrwnekd <34386511+dbstjrwnekd@users.noreply.github.com>
2020-12-01 14:06:52 +0900

    Update index.tsx

--- fe/src/pages/LoginPage/index.tsx
+++ fe/src/pages/LoginPage/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // import { useHistory } from 'react-router-dom'
-import LoginTemplate from '../../components/templates/LoginTemplate';
-import LoginButtons from '../../components/organisms/LoginButtons';
-import Header from '../../components/organisms/Header';
+import LoginTemplate from 'components/templates/LoginTemplate';
+import LoginButtons from 'components/organisms/LoginButtons';
+import Header from 'components/organisms/Header';
 
 function LoginPage(): React.ReactElement {
   // const history = useHistory()
