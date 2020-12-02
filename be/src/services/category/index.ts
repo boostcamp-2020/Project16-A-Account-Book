fb19d366c9ef4bceb28f049a0126984e91b07508
yejineee <yygeniee@gmail.com>
2020-12-02 23:49:35 +0900

    [BE] Feat: category statistics 보내는 API 완성
    수입, 지출별 총 금액과 수입과 지출 카테고리별 총 금액을 보내는 API를 완성하였다.

--- be/src/services/category/index.ts
+++ be/src/services/category/index.ts
@@ -1,15 +1,33 @@
 import { AccountModel } from 'models/account';
 import { categoryType } from 'models/category';
 
-const sumPricesByType = (transactions: Array<any>) => {
-  const initTotalPrice = { income: 0, expense: 0 };
-  const totalPrice = transactions.reduce((sumPrice, transaction) => {
-    if (transaction.category.type === categoryType.EXPENSE) {
-      return { ...sumPrice, expense: sumPrice.expense + transaction.price };
+const getSumByCategories = (transactions: Array<any>) => {
+  const initState = {
+    totalPrice: { income: 0, expense: 0 },
+    incomeCategories: {},
+    expenseCategories: {},
+  };
+  const reducer = (state: any, transaction: any) => {
+    const copyState = state;
+    const { type, title, _id: id } = transaction.category;
+    const [totalType, catType] =
+      type === categoryType.EXPENSE
+        ? ['expense', 'expenseCategories']
+        : ['income', 'incomeCategories'];
+    copyState.totalPrice[totalType] += transaction.price;
+    if (copyState[catType][title]) {
+      copyState[catType][title].totalPrice += transaction.price;
+    } else {
+      copyState[catType][title] = {
+        id,
+        title,
+        color: transaction.color,
+        totalPrice: transaction.price,
+      };
     }
-    return { ...sumPrice, income: sumPrice.income + transaction.price };
-  }, initTotalPrice);
-  return totalPrice;
+    return copyState;
+  };
+  return transactions.reduce(reducer, initState);
 };
 
 export const getCategoryStatistics = async (
@@ -22,9 +40,7 @@ export const getCategoryStatistics = async (
     startDate,
     endDate,
   );
-
-  const totalPrice = sumPricesByType(transactions);
-  return { totalPrice };
+  return getSumByCategories(transactions);
 };
 
 export default {};
