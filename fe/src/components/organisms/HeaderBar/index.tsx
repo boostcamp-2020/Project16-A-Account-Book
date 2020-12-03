5c424a016189b4f232562d17cc50f893375dd665
pkiop <pkiop@naver.com>
2020-12-03 23:34:22 +0900

    [FE] Feat: HeaderBar에 가계부 라우팅 기능 추가

--- fe/src/components/organisms/HeaderBar/index.tsx
+++ fe/src/components/organisms/HeaderBar/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { TransactionStore } from 'stores/Transaction';
 import * as S from './style';
 
 export interface Props {}
 
+const onClickHandler = () => {
+  TransactionStore.setAccountTitle('N석봉');
+  TransactionStore.setAccountObjId('empty');
+};
+
 const HeaderBar = ({ ...props }: Props): React.ReactElement => {
-  return <S.HeaderBar {...props} />;
+  return (
+    <S.HeaderBar {...props}>
+      <Link to="/" onClick={onClickHandler}>
+        {TransactionStore.accountTitle}
+      </Link>
+    </S.HeaderBar>
+  );
 };
 
 export default HeaderBar;
--- fe/src/components/organisms/HeaderBar/style.ts
+++ fe/src/components/organisms/HeaderBar/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export interface Props {}
 
@@ -6,4 +7,13 @@ export const HeaderBar = styled.div`
   width: 100%;
   height: 3rem;
   background-color: ${({ theme }) => theme.color.brandColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LinkTag = styled(Link)`
+  :hover {
+    background-color: green;
+  }
 `;
