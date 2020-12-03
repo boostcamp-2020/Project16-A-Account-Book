6beb39acd7d748b5d3c60e5e89fd87383a863d2d
yejineee <yygeniee@gmail.com>
2020-12-04 06:52:10 +0900

    [FE] Feat: 파이차트 스토리 추가
    파이차트에 다양한 상황을 위한 스토리를 추가하였다.

--- fe/src/components/molecules/PieChart/index.stories.tsx
+++ fe/src/components/molecules/PieChart/index.stories.tsx
@@ -13,14 +13,60 @@ export const SimplePieChart = () => {
       color: '#be00a5',
       _id: '5fc79e46233d1b4e239f28df',
       totalPrice: 1445389,
-      percent: 64,
+      percent: 70,
     },
     {
       title: '사업수입',
       color: '#dded7e',
       _id: '5fc79e46233d1b4e239f28de',
       totalPrice: 803999,
-      percent: 36,
+      percent: 10,
+    },
+  ];
+  return (
+    <GlobalThemeProvider>
+      <PieChart pieces={pieces} />
+    </GlobalThemeProvider>
+  );
+};
+export const SimplePieChart2 = () => {
+  const pieces = [
+    {
+      title: '기타수입',
+      color: '#be00a5',
+      _id: '5fc79e46233d1b4e239f28df',
+      totalPrice: 1445389,
+      percent: 50,
+    },
+    {
+      title: '사업수입',
+      color: '#dded7e',
+      _id: '5fc79e46233d1b4e239f28de',
+      totalPrice: 803999,
+      percent: 20,
+    },
+  ];
+  return (
+    <GlobalThemeProvider>
+      <PieChart pieces={pieces} />
+    </GlobalThemeProvider>
+  );
+};
+export const SimplePieChart3 = () => {
+  const pieces = [
+    {
+      title: '기타수입',
+      color: '#be00a5',
+      _id: '5fc79e46233d1b4e239f28df',
+      totalPrice: 1445389,
+      percent: 100,
+    },
+    {
+      title: '사업수입',
+      color: '#dded7e',
+      _id: '5fc79e46233d1b4e239f28de',
+      totalPrice: 803999,
+      percent: 0,
     },
   ];
   return (
