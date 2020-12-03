212bd32eb2d49bce6271abb9558d692dd48dc496
pkiop <pkiop@naver.com>
2020-12-03 23:27:58 +0900

    [BE] Feat: Account API 관련 라우팅

--- be/src/routers/api/index.ts
+++ be/src/routers/api/index.ts
@@ -3,12 +3,14 @@ import transactionRouter from 'routers/api/transaction';
 import categoryRouter from 'routers/api/category';
 import authRouter from 'routers/api/auth';
 import userRouter from 'routers/api/user';
+import accountRouter from 'routers/api/account';
 import methodRouter from './method';
 
 const router = new Router();
 
 router.use('/categories', categoryRouter.routes());
 router.use('/transactions', transactionRouter.routes());
+router.use('/accounts', accountRouter.routes());
 router.use('/auth', authRouter.routes());
 router.use('/method', methodRouter.routes());
 router.use('/user', userRouter.routes());
