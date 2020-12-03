c5e8485402056adc04f2019234aa0b9a69f3617f
rolled-potatoes <keep4404@gmail.com>
2020-12-04 03:05:33 +0900

    [FE] Chore : 의존성 문제 해결 및 환경변수 설정

--- be/webpack.config.js
+++ be/webpack.config.js
@@ -1,37 +1,44 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
+const dotenv = require('dotenv');
 
-module.exports = {
-  name: 'ts-back-setting',
-  mode: 'development', // "production" | "development" | "none"
-  devtool: 'eval', // source-map hidden-source-map
-  externals: [nodeExternals()],
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.ts', 'json', '.jsx', '.js'],
-    plugins: [
-      new TsconfigPathsPlugin({
-        configFile: 'tsconfig.json',
-      }),
-    ],
-  },
-  entry: {
-    index: ['./src/app.ts'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: ['ts-loader'],
-        exclude: ['/node_modules'],
-      },
-    ],
-  },
-  optimization: {},
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: '[name].js',
-  },
-  target: 'node',
+module.exports = (env, options) => {
+  dotenv.config({
+    path: './.env',
+  });
+  return {
+    name: 'ts-back-setting',
+    mode: 'development', // "production" | "development" | "none"
+    devtool: 'eval', // source-map hidden-source-map
+    externals: [nodeExternals()],
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.ts', 'json', '.jsx', '.js'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: 'tsconfig.json',
+        }),
+      ],
+    },
+    plugins: [],
+    entry: {
+      index: ['./src/app.ts'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: ['ts-loader'],
+          exclude: ['/node_modules'],
+        },
+      ],
+    },
+    optimization: {},
+    output: {
+      path: path.join(__dirname, './dist'),
+      filename: '[name].js',
+    },
+    target: 'node',
+  };
 };
