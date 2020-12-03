fd8e15cb8c08fcb162e061f3c5aee318212490a6
rolled-potatoes <keep4404@gmail.com>
2020-12-03 16:46:39 +0900

    [FE] Refactor : 라우팅 경로 수정
    서브라우팅을 활용하여 라우팅 분리 함.

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,23 +1,64 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+} from 'react-router-dom';
 import GlobalThemeProvider from 'styles/GlobalThemeProvider';
+import AuthCheck from 'pages/AuthCheck';
 import LoginPage from './pages/LoginPage';
 import OauthCallbackPage from './pages/OauthCallbackPage';
 import MainPage from './pages/MainPage';
 import CreateTransactionPage from './pages/CreateTransactionPage';
 
+const TransactionRouter = () => {
+  const { url } = useRouteMatch();
+  return (
+    <>
+      <AuthCheck />
+      <Switch>
+        <Route path={`${url}/create`} component={CreateTransactionPage} />
+        <Route path={url} component={MainPage} />
+      </Switch>
+    </>
+  );
+};
+
+const LoginRouter = () => {
+  const { url } = useRouteMatch();
+  return (
+    <>
+      <Switch>
+        <Route
+          exact
+          path={`${url}/oauth-callback`}
+          component={OauthCallbackPage}
+        />
+        <Route path={`${url}`} component={LoginPage} />
+      </Switch>
+    </>
+  );
+};
+
+const AccountRouter = () => {
+  return (
+    <>
+      <AuthCheck />
+      가계부 선택
+    </>
+  );
+};
+
 const App = () => {
   return (
     <GlobalThemeProvider>
+      <AuthCheck />
       <Router>
         <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/oauth-callback" component={OauthCallbackPage} />
-          <Route
-            path="/:account/transactions/create"
-            component={CreateTransactionPage}
-          />
-          <Route path="/:account" component={MainPage} />
+          <Route path="/login" component={LoginRouter} />
+          <Route path="/accounts" component={AccountRouter} />
+          <Route path="/transactions/:title" component={TransactionRouter} />
         </Switch>
       </Router>
     </GlobalThemeProvider>
