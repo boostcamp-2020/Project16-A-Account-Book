c8c5d05d2356dfb27ef611302ccf451aba6904aa
rolled-potatoes <keep4404@gmail.com>
2020-12-04 02:13:06 +0900

    [BE] Chore : 번들링, 절대 경로 문제 수정

--- be/webpack.config.js
+++ be/webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   name: 'ts-back-setting',
   mode: 'development', // "production" | "development" | "none"
   devtool: 'eval', // source-map hidden-source-map
+  externals: [nodeExternals()],
   resolve: {
     modules: ['node_modules'],
     extensions: ['.ts', 'json', '.jsx', '.js'],
     plugins: [
       new TsconfigPathsPlugin({
         configFile: 'tsconfig.json',
-        baseUrl: './',
       }),
     ],
   },
