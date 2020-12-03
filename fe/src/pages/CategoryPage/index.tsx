cef44583cf51d42ea28a3a6c2e8f60d29e641d14
dbstjrwnekd <sh4869@ajou.ac.kr>
2020-12-03 13:57:30 +0900

    Feat : 카테고리 페이지 스토어 수정

--- fe/src/pages/CategoryPage/index.tsx
+++ fe/src/pages/CategoryPage/index.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CategoryTemplate from 'components/templates/CategoryTemplate';
 import Header from 'components/organisms/Header';
 import backArrow from 'assets/svg/backArrow.svg';
 import IconButton from 'components/molecules/IconButton';
 import CategoryArea from 'components/organisms/CategoryArea';
-import { observer } from 'mobx-react';
-import { CategoryStore } from 'stores/Category';
+import { observer } from 'mobx-react-lite';
+import { CategoryStore, categoryType } from 'stores/Category';
 
 function CategoryPage(): React.ReactElement {
+  const [type, setType] = useState(categoryType.EXPENSE);
+
   useEffect(() => {
     CategoryStore.loadCategories();
   }, []);
@@ -17,7 +19,7 @@ function CategoryPage(): React.ReactElement {
       headerContent={<Header />}
       homeButton={<IconButton icon={backArrow} />}
       title="카테고리 설정"
-      bodyContent={<CategoryArea dataList={CategoryStore.dataList} />}
+      bodyContent={<CategoryArea dataList={CategoryStore.categoryList[type]} />}
     />
   );
 }
--- fe/src/pages/MainPage/index.tsx
+++ fe/src/pages/MainPage/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { toJS } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer } from 'mobx-react-lite';
 import { TransactionStore } from 'stores/Transaction';
 import Template from 'components/templates/HeaderNav';
 import Header from 'components/organisms/HeaderBar';
