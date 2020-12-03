b340d7f975e7a1ee963bd63614e797c9035a7e54
pkiop <pkiop@naver.com>
2020-12-03 16:59:44 +0900

    [FE] Fix: Accountobjid 참조해서 transaction 가져오도록 수정

--- be/src/services/transaction/index.ts
+++ be/src/services/transaction/index.ts
@@ -1,6 +1,31 @@
 import { TransactionModel, ITransaction } from 'models/transaction';
 import { AccountModel } from 'models/account';
 
+const findTransactionsAndBindWithDateKey = (
+  startDate: string,
+  endDate: string,
+) => async (acc: any, transactionId: string) => {
+  const res = await TransactionModel.findOne({ _id: transactionId })
+    .populate('category')
+    .populate('method')
+    .where('date')
+    .gte(new Date(startDate))
+    .lt(new Date(endDate));
+  if (!res) {
+    return acc;
+  }
+  const resDate = res.date;
+  const resKey = `${resDate.getFullYear()}-${
+    resDate.getMonth() + 1
+  }-${resDate.getDate()}`;
+
+  if (acc[resKey]) {
+    return acc[resKey].push(res);
+  }
+  acc[resKey] = res;
+  return { ...acc };
+};
+
 const oneMonthTransactionsReducer = (acc: any, transaction: ITransaction) => {
   const year = transaction.date.getFullYear();
   const month = transaction.date.getMonth() + 1;
@@ -14,19 +39,36 @@ const oneMonthTransactionsReducer = (acc: any, transaction: ITransaction) => {
 export const getTransaction = async ({
   startDate,
   endDate,
+  accountObjId,
 }: {
   startDate: string;
   endDate: string;
+  accountObjId: string;
 }) => {
-  const oneMonthTransactions: ITransaction[] = await TransactionModel.find()
-    .populate('category')
-    .populate('method')
-    .where('date')
-    .gte(new Date(startDate))
-    .lt(new Date(endDate))
-    .sort('date');
+  const res = await AccountModel.findOne({
+    _id: accountObjId,
+  })
+    .populate({
+      path: 'transactions',
+      match: { date: { $gte: startDate, $lt: endDate } },
+      populate: { path: 'category methods' },
+    })
+    .exec();
 
-  const result = oneMonthTransactions.reduce(oneMonthTransactionsReducer, {});
+  if (!res) {
+    return { message: 'nodata' };
+  }
+
+  const trans = await res.transactions;
+  if (trans.length === 0) {
+    return { message: 'nodata' };
+  }
+
+  trans.sort((firstEl: any, secondEl: any) => {
+    return firstEl.date - secondEl.date;
+  });
+
+  const result = trans.reduce(oneMonthTransactionsReducer, {});
   return result;
 };
 
