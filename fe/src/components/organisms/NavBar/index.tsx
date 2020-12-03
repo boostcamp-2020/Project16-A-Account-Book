3cbbbb660bf848d2a436df23bad8a22316d77228
rolled-potatoes <keep4404@gmail.com>
2020-12-03 16:50:33 +0900

    [FE] Feat : 메인페이지에서 생성 버튼 라우팅

--- fe/src/components/organisms/NavBar/index.tsx
+++ fe/src/components/organisms/NavBar/index.tsx
@@ -5,14 +5,18 @@ import graphSvg from 'assets/svg/graph.svg';
 import calendarSvg from 'assets/svg/calendar.svg';
 import tagSvg from 'assets/svg/tag.svg';
 import plusSvg from 'assets/svg/plus.svg';
+import { Link } from 'react-router-dom';
 import * as S from './style';
 
 const NavBar = ({ ...props }): React.ReactElement => {
+  const { title } = JSON.parse(sessionStorage.getItem('account') || '');
   return (
     <S.NavBar {...props}>
       <IconButton icon={homeSvg}>홈</IconButton>
       <IconButton icon={graphSvg}>통계</IconButton>
-      <S.PlusButton icon={plusSvg}>+</S.PlusButton>
+      <Link to={`/transactions/${title}/create`}>
+        <S.PlusButton icon={plusSvg} />
+      </Link>
       <IconButton icon={calendarSvg}>달력</IconButton>
       <IconButton icon={tagSvg}>태그</IconButton>
     </S.NavBar>
