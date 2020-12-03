5c5df022b8a927110b05bc52e18991557d7bc0c0
pkiop <pkiop@naver.com>
2020-12-03 23:34:53 +0900

    [FE] Fix: 달력 페이지 라우팅
    * 승환, 예진님 코드랑 겹칠 것 같은데 제꺼 버리시면 될 것 같습니다!

--- fe/src/components/organisms/NavBar/index.tsx
+++ fe/src/components/organisms/NavBar/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { toJS } from 'mobx';
 import IconButton from 'components/molecules/IconButton';
 import homeSvg from 'assets/svg/home.svg';
 import graphSvg from 'assets/svg/graph.svg';
 import calendarSvg from 'assets/svg/calendar.svg';
 import tagSvg from 'assets/svg/tag.svg';
 import plusSvg from 'assets/svg/plus.svg';
+import { TransactionStore } from 'stores/Transaction';
+import { useHistory } from 'react-router-dom';
 import * as S from './style';
 
+const onClickHandler = (history: any, title: string) =>
+  useCallback(() => {
+    history.push(`/${title}/calender`);
+  }, [toJS(TransactionStore.dates)]);
+
 const NavBar = ({ ...props }): React.ReactElement => {
+  const history = useHistory();
   return (
     <S.NavBar {...props}>
       <IconButton icon={homeSvg}>홈</IconButton>
       <IconButton icon={graphSvg}>통계</IconButton>
       <S.PlusButton icon={plusSvg}>+</S.PlusButton>
-      <IconButton icon={calendarSvg}>달력</IconButton>
+      <IconButton
+        icon={calendarSvg}
+        onClick={onClickHandler(history, TransactionStore.accountTitle)}
+      >
+        달력
+      </IconButton>
       <IconButton icon={tagSvg}>태그</IconButton>
     </S.NavBar>
   );
