dab3299606d57fd636530400cff91d95d77136c4
yejineee <yygeniee@gmail.com>
2020-12-07 03:58:04 +0900

    [FE] Feat: 라인 차트에서 날짜 포맷 변경 함수 추가
    전체 트랜잭션 중에서 년이 달라진다면, 년월일로 날짜 포맷을 변경한다.
    만약, 년이 같다면 년을 표시하지 않고, 월일로 날짜 포맷을 변경한다.

--- fe/src/components/organisms/LineChartOverview/index.tsx
+++ fe/src/components/organisms/LineChartOverview/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import LineChart from 'components/molecules/LineChart';
 import { TransactionStore } from 'stores/Transaction';
 import { observer } from 'mobx-react-lite';
-import dayjs from 'dayjs';
 import { IDateTotalprice } from 'types';
 import dateUtils from 'utils/date';
 import * as S from './style';
@@ -20,24 +19,38 @@ const addDataIfScarce = (dataList: IDateTotalprice[]) => {
 const isAllZeroPrice = (dataList: IDateTotalprice[]) =>
   dataList.every((data) => data.totalPrice === 0);
 
+const formalizeDate = (dataList: IDateTotalprice[]) => {
+  if (dataList.length < 1) return [];
+  const YEAR_FORMAT = 'YYYY';
+  const yearOfFirstData = dateUtils.dateCustomFormatter(
+    dataList[0].date,
+    YEAR_FORMAT,
+  );
+  const yearOfLastData = dateUtils.dateCustomFormatter(
+    dataList[dataList.length - 1].date,
+    YEAR_FORMAT,
+  );
+  const FORMAT = yearOfFirstData !== yearOfLastData ? 'YY/MM/DD' : 'MM/DD';
+  return dataList.map((data) => ({
+    ...data,
+    date: dateUtils.dateCustomFormatter(data.date, FORMAT),
+  }));
+};
+
 const LineChartOverview = (): React.ReactElement => {
   const dataList = addDataIfScarce(TransactionStore.totalExpensePriceByDate);
-  const formattedDataList = dataList.map((data) => ({
-    ...data,
-    date: dayjs(data.date).format('MM/DD'),
-  }));
   const EmptyData = <div>소비를 하지 않으셨네요!</div>;
 
   return (
     <div>
       <S.StatisticsTitle>지출 추이</S.StatisticsTitle>
-      {isAllZeroPrice(dataList) ? (
+      {dataList.length > 0 && isAllZeroPrice(dataList) ? (
         EmptyData
       ) : (
         <LineChart
-          width={200}
-          height={150}
-          data={formattedDataList}
+          width={250}
+          height={100}
+          data={formalizeDate(dataList)}
           horizontalGuides={Math.min(dataList.length, 5)}
         />
       )}
