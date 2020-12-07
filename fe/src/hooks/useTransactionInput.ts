50ea8cb2fd237ae2f80ee203cfc532c602dea955
yejineee <yygeniee@gmail.com>
2020-12-08 00:04:02 +0900

    [FE] Fix: 결제 수단 선택 안할시 제출 안되는 문제 해결
    결제수단을 로드해온 후, 첫 번째 값을 결제수단의 초기값으로 설정하여,
    결제수단을 선택하지 않아도 기본적으로 첫 번째 결제수단이 들어가도록 하여 해결하였다.

--- fe/src/hooks/useTransactionInput.ts
+++ fe/src/hooks/useTransactionInput.ts
@@ -17,7 +17,7 @@ const initState = {
   client: '',
   memo: '',
   price: 0,
-  classification: '',
+  classification: '지출',
   category: '미분류',
   method: '',
 };
@@ -30,7 +30,14 @@ const useTransactionInput = (transactionObjId?: string): [State, any] => {
       [name]: value,
     }));
   };
-
+  const loadAndSetInitialMethod = async () => {
+    await MethodStore.loadMethods();
+    const initialMethod = MethodStore.getMethods()[0];
+    setTransaction((prevState) => ({
+      ...prevState,
+      method: initialMethod._id,
+    }));
+  };
   useEffect(() => {
     const { classification } = transactionState;
     const defaultCategory = CategoryStore.getCategories(classification)[0];
@@ -43,7 +50,7 @@ const useTransactionInput = (transactionObjId?: string): [State, any] => {
 
   useEffect(() => {
     CategoryStore.loadCategories();
-    MethodStore.loadMethods();
+    loadAndSetInitialMethod();
     if (transactionObjId) {
       // TODO: transaction 하나의 data 가져와서, initState에 넣어주기
     }
