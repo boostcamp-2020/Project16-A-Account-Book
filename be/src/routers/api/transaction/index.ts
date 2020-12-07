7ec52a160d58e983c54db943e56fd130f6af546c
yejineee <yygeniee@gmail.com>
2020-12-07 17:14:00 +0900

    [BE] Feat: 하나의 트랜잭션 가져오는 API의  라우터 설정
    하나의 트랜잭션을 가져오는 API는
    get /transactions/:transactionObjId/detail 이다.
    해당 url의 라우팅을 설정하였다.

--- be/src/routers/api/transaction/index.ts
+++ be/src/routers/api/transaction/index.ts
@@ -3,6 +3,7 @@ import transactionController from 'controllers/transaction';
 
 const router = new Router();
 
+router.get('/:transactionObjId/detail', transactionController.getTransaction);
 router.get('/:accountObjId', transactionController.getTransactionList);
 router.post('/:accountObjId', transactionController.post);
 export default router;
