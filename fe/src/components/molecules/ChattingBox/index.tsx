2fd9d7f948faadc148f6cb1f28eb5fad8a82ae78
dbstjrwnekd <sh4869@ajou.ac.kr>
2020-12-08 01:11:03 +0900

    [FE] 채팅페이지 UI
    - 채팅페이지 UI구현 (Corpus, ChatBox, Chatting Area)
    - 페이지에서 사용하면서 스토어 연결 필요
    - 데이터 타입에 관한 논의 필요

--- fe/src/components/molecules/ChattingBox/index.tsx
+++ fe/src/components/molecules/ChattingBox/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import Corpus from 'components/atoms/Corpus';
 import * as S from './style';
 
-const ChattingBox = (): React.ReactElement => {
+const myMessage = 'mine';
+
+export interface DataProps {
+  type: string;
+  value: string;
+}
+
+export interface ChattingBoxProps {
+  dataList: DataProps[];
+}
+
+const ChattingBox = ({ dataList }: ChattingBoxProps): React.ReactElement => {
   return (
     <S.ChattingBox>
-      <S.ChattingArea />
+      <S.ChattingArea>
+        {dataList.map(
+          (data: DataProps): React.ReactElement =>
+            data.type === myMessage ? (
+              <S.MyMessage>
+                <Corpus type={data.type} chat={data.value} />
+              </S.MyMessage>
+            ) : (
+              <S.ServerMessage>
+                <Corpus type={data.type} chat={data.value} />
+              </S.ServerMessage>
+            ),
+        )}
+      </S.ChattingArea>
     </S.ChattingBox>
   );
 };
--- fe/src/components/organisms/ChattingArea/index.tsx
+++ fe/src/components/organisms/ChattingArea/index.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import { DataProps } from 'components/molecules/ChattingBox';
 import * as S from './style';
-// props 수정예정
-const onChangeHandler = () => {
-  console.log('input changed!');
-};
 
-const ChattingArea = () => {
+export interface ChattingAreaProps {
+  onChangeHandler: any;
+  onSubmitHandler: any;
+  dataList: DataProps[];
+}
+
+const ChattingArea = ({
+  onChangeHandler,
+  onSubmitHandler,
+  dataList,
+}: ChattingAreaProps) => {
   return (
     <S.ChattingArea>
-      <S.ChatBox />
+      <S.ChatBox dataList={dataList} />
       <S.ChattingBottom>
         <S.ChattingInput
           disabled={false}
           placeholder="chat your message"
           onChangeHandler={onChangeHandler}
         />
-        <S.ChattingBtn>전송</S.ChattingBtn>
+        <S.ChattingBtn
+          onClick={onSubmitHandler}
+          disabled={false}
+          value="전송"
+          type="button"
+        />
       </S.ChattingBottom>
     </S.ChattingArea>
   );
