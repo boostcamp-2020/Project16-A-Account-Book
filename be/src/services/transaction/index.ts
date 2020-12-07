4130ae104ec7d63ea8ff67c931e35e0c28ab2d78
yejineee <yygeniee@gmail.com>
2020-12-07 16:55:57 +0900

    [BE] Fix: 사용하지 않는 함수 삭제
    트랜잭션 서비스 코드 중에서 사용하지 않는 함수들을 삭제한다.

--- be/src/services/transaction/index.ts
+++ be/src/services/transaction/index.ts
@@ -1,6 +1,5 @@
 import { TransactionModel, ITransaction } from 'models/transaction';
 import { AccountModel } from 'models/account';
-import { categoryType } from 'models/category';
 import { getCompFuncByKey } from 'libs/utils';
 
 const oneMonthTransactionsReducer = (acc: any, transaction: ITransaction) => {
@@ -13,18 +12,6 @@ const oneMonthTransactionsReducer = (acc: any, transaction: ITransaction) => {
     : { ...acc, [key]: [transaction] };
 };
 
-const sumPricesByType = (transactions: Array<any>) => {
-  const initTotalPrice = { income: 0, expense: 0 };
-  const totalPrice = transactions.reduce((sumPrice, transaction) => {
-    if (transaction.category.type === categoryType.EXPENSE) {
-      return { ...sumPrice, expense: sumPrice.expense + transaction.price };
-    }
-    return { ...sumPrice, income: sumPrice.income + transaction.price };
-  }, initTotalPrice);
-  return totalPrice;
-};
-
-const sumPricesByCategory = (transactions: Array<any>) => {};
 export const getTransaction = async ({
   startDate,
   endDate,
@@ -71,18 +58,4 @@ export const saveAndAddToAccount = async (
     accountObjId,
     transcationObjId,
   );
-};
-export const getCategoryStatistics = async (
-  accountObjId: string,
-  startDate: string,
-  endDate: string,
-) => {
-  const transactions = await AccountModel.findByPkAndGetTransCategory(
-    accountObjId,
-    startDate,
-    endDate,
-  );
-  const totalPrice = sumPricesByType(transactions);
-  const categories = sumPricesByCategory(transactions);
-  return { totalPrice };
 };
