b6705f8038f8a998ed1117f12ebae564c9cf4712
pkiop <pkiop@naver.com>
2020-12-07 18:20:51 +0900

    [FE] Fix: 주석 제거

--- fe/src/components/organisms/CalenderBind/index.stories.tsx
+++ fe/src/components/organisms/CalenderBind/index.stories.tsx
@@ -97,52 +97,7 @@ const testAccountDateList = {
       __v: 0,
     },
   ],
-  // '2020-11': [],
-  // '2020-11-12': [
-  //   {
-  //     excludeFromBudget: false,
-  //     _id: 'aaa',
-  //     client: '삼겹살',
-  //     date: '2020-11-12T00:00:00.000Z',
-  //     memo: '1일메모',
-  //     method: {
-  //       _id: 'naver',
-  //       title: '네이버페이',
-  //       __v: 0,
-  //     },
-  //     category: {
-  //       _id: 'incomeid',
-  //       type: 'INCOME',
-  //       title: '식사',
-  //       color: '#102e94',
-  //       __v: 0,
-  //     },
-  //     price: 10000,
-  //     __v: 0,
-  //   },
 
-  //   {
-  //     excludeFromBudget: false,
-  //     _id: 'bbb',
-  //     client: '삼겹살d',
-  //     date: '2020-11-12T00:20:00.000Z',
-  //     memo: '1일메모',
-  //     method: {
-  //       _id: 'naver',
-  //       title: '네이버페이',
-  //       __v: 0,
-  //     },
-  //     category: {
-  //       _id: 'incomeid',
-  //       type: 'EXPENSE',
-  //       title: '식사',
-  //       color: '#102e94',
-  //       __v: 0,
-  //     },
-  //     price: 10000,
-  //     __v: 0,
-  //   },
-  // ],
   '2020-12-12': [
     {
       excludeFromBudget: false,
