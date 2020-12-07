993c3a661ff94b31c1d26ab6297df1b216f508d8
yejineee <yygeniee@gmail.com>
2020-12-08 00:43:26 +0900

    [FE] Feat: 하나의 트랜잭션 가져오는 axios 함수 구현
    하나의 트랜잭션을 가져오기 위해, 인자로 transaction object id를 받고
    해당 url에 object id를 담아서 get 메소드로 요청한다.

--- fe/src/apis/transaction.ts
+++ fe/src/apis/transaction.ts
@@ -4,6 +4,9 @@ import axios from './axios';
 import url from './urls';
 
 export default {
+  getTransaction(transactionObjId: string) {
+    return axios.get(url.transactionDetail(transactionObjId));
+  },
   getTransactionList(accountObjId: string, query: object) {
     return axios.get(
       `${url.transaction(accountObjId)}${makeQueryString(query)}`,
