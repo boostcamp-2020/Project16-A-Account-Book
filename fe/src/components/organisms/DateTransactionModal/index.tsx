97cdeb8b01ac3706ad361a82ca0a9e4428dfa8bd
pkiop <pkiop@naver.com>
2020-12-07 22:47:16 +0900

    [FE] Feat: 달력 모달 컴포넌트 작성

---
+++ fe/src/components/organisms/DateTransactionModal/index.tsx
@@ -0,0 +1,32 @@
+import React, { useRef, forwardRef } from 'react';
+import { TransactionStore } from 'stores/Transaction';
+import * as S from './style';
+
+export interface Props {}
+
+const onClickHandler = (contentRef: React.RefObject<HTMLDivElement>) => (
+  e: React.MouseEvent<HTMLDivElement>,
+) => {
+  e.preventDefault();
+  if (e.target === contentRef.current) {
+    return;
+  }
+  TransactionStore.setModalVisible(false);
+};
+
+const DateTransactionModal = forwardRef(
+  ({ ...props }: Props, ref: any): React.ReactElement => {
+    const contentRef = useRef<HTMLDivElement>(null);
+    return (
+      <S.DateTransactionModal
+        onClick={onClickHandler(contentRef)}
+        ref={ref}
+        {...props}
+      >
+        <S.Content ref={contentRef} />
+      </S.DateTransactionModal>
+    );
+  },
+);
+
+export default DateTransactionModal;
