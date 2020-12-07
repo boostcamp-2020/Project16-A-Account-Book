676d86de912c9788b8a2fda62f3b938fff71e1d2
yejineee <yygeniee@gmail.com>
2020-12-08 02:43:05 +0900

    [FE] Feat: 메인 페이지에서 거래내역 클릭시 거래내역 수정 페이지로 이동
    거래내역 클릭 시, 거래내역 id를 파라미터로 전달하여 해당 id를 쿼리스트링으로 넘겨서
    거래내역 수정 페이지로 이동하게 한다.

--- fe/src/pages/MainPage/index.tsx
+++ fe/src/pages/MainPage/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { TransactionStore } from 'stores/Transaction';
+import { useHistory, useParams } from 'react-router-dom';
+
 import Template from 'components/templates/MainTemplate';
 import Header from 'components/organisms/HeaderBar';
 import FilterBar from 'components/organisms/FilterBar';
@@ -10,6 +12,9 @@ import NavBarComponent from 'components/organisms/NavBar';
 import TransactionDateList from './TransactionDateList';
 
 const MainPage = () => {
+  const history = useHistory();
+  const { title } = useParams<{ title: string }>();
+
   useEffect(() => {
     TransactionStore.loadTransactions();
   }, []);
@@ -21,10 +26,17 @@ const MainPage = () => {
     />
   );
 
+  const onClickHandler = (id: string) => {
+    history.push(`/transactions/${title}/update?transactionObjId=${id}`);
+  };
+
   const Contents = (
     <div>
       <FilterBar />
-      <TransactionDateList list={toJS(TransactionStore.transactions)} />
+      <TransactionDateList
+        list={toJS(TransactionStore.transactions)}
+        onClick={onClickHandler}
+      />
     </div>
   );
 
