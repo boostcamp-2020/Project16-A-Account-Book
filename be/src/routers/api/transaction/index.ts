39c578947b593f01fdcbc2da8244d1ef71c9436a
yejineee <yygeniee@gmail.com>
2020-12-08 02:57:51 +0900

    [FE] Feat: 거래내역 수정 라우팅 추가
    거래내역 수정은 put method이며, transactionObjId를 파라미터로 전달하게 된다.

--- be/src/routers/api/transaction/index.ts
+++ be/src/routers/api/transaction/index.ts
@@ -6,4 +6,5 @@ const router = new Router();
 router.get('/:transactionObjId/detail', transactionController.getTransaction);
 router.get('/:accountObjId', transactionController.getTransactionList);
 router.post('/:accountObjId', transactionController.post);
+router.put('/:transactionObjId', transactionController.updateTransaction);
 export default router;
