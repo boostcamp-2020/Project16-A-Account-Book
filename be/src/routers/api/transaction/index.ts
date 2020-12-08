86ff66018008e8db5890657836c20c80e258b41d
yejineee <yygeniee@gmail.com>
2020-12-09 00:11:57 +0900

    [BE] Feat: 거래내역 삭제 API 추가
    거래내역 Object id를 받아서, 해당 거래내역을 삭제한다.
    이 때, soft delete를 하기 때문에, isDeleted필드를 true로 바꿔준다.

--- be/src/routers/api/transaction/index.ts
+++ be/src/routers/api/transaction/index.ts
@@ -3,12 +3,12 @@ import transactionController from 'controllers/transaction';
 
 const router = new Router();
 
+router.delete('/:transactionObjId', transactionController.deleteTransaction);
 router.get('/detail/:transactionObjId', transactionController.getTransaction);
 router.put(
   '/update/:transactionObjId',
   transactionController.updateTransaction,
 );
-router.delete(':transactionObjId', transactionController.deleteTransaction);
 router.get('/', transactionController.getTransactionList);
 router.post('/', transactionController.post);
 export default router;
