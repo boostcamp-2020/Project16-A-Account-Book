70c866f00c5e9486c16dea41d35e149a84657b2f
rolled-potatoes <keep4404@gmail.com>
2020-12-08 14:12:25 +0900

    [BE] Feat : jwt 인증 미들웨어 생성

---
+++ be/src/middlewares/index.ts
@@ -0,0 +1,36 @@
+import Koa from 'koa';
+import jwt from 'jsonwebtoken';
+import { jwtConfig } from 'config';
+import { unAuthroziedError } from 'libs/error';
+import { UserModel } from 'models/user';
+
+interface IDecodedData {
+  id: string | number;
+}
+
+export const authorization = async (
+  ctx: Koa.Context,
+  next: () => Promise<any>,
+) => {
+  try {
+    const key = 'access_token';
+    const accessToken = ctx.cookies.get(key);
+
+    if (!accessToken) {
+      throw unAuthroziedError;
+    }
+    const decodedData = jwt.verify(
+      accessToken,
+      jwtConfig.jwtSecret,
+    ) as IDecodedData;
+    const user = await UserModel.findOne({ id: decodedData.id });
+    if (!user) {
+      throw unAuthroziedError;
+    }
+    ctx.user = user;
+    next();
+  } catch (e) {
+    throw unAuthroziedError;
+  }
+};
+export default {};
