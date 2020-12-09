40d66b9e30b12cbfab8666d6afd10f3a5f536d71
yejineee <yygeniee@gmail.com>
2020-12-09 21:20:02 +0900

    [FE] Refactor: 필터링 후, 타입을 바꿔주는 것으로 수정
    필터링을 해주는 함수를 따로 분리하였으므로,
    필터링이 되어있다면, 거래내역 리스트를 필터링한 후
    디비 타입을 바꿔주는 것으로 하였다.

--- fe/src/pages/MainPage/TransactionDateList.tsx
+++ fe/src/pages/MainPage/TransactionDateList.tsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import TransactionList from 'components/organisms/TransactionList';
 import { TransactionStore } from 'stores/Transaction';
+import {
+  filterList,
+  convertTransactionDBTypetoTransactionType,
+} from 'stores/Transaction/transactionStoreUtils';
 import { observer } from 'mobx-react-lite';
-import { convertTransactionDBTypetoTransactionType } from 'stores/Transaction/transactionStoreUtils';
-import dateUtils from 'utils/date';
-
-type TransactionDBKeyValue = [date: string, transactions: any];
+import * as types from 'types';
 
 const TransactionDateList = ({
   list,
   onClick,
 }: {
-  list: any;
+  list: types.IDateTransactionObj;
   onClick: any;
 }) => {
-  const { startDate, endDate } = TransactionStore.getOriginDates();
-  const mapFunc = (item: TransactionDBKeyValue) => {
-    const [date, transactions] = item;
-    // const transactionList = TransactionStore.isFiltered? TransactionStore.getTransactions():
-    if (
-      TransactionStore.isFiltered &&
-      !dateUtils.isDateInDateRange(new Date(date), startDate, endDate)
-    )
-      return '';
-
-    const filteredTransactionList = convertTransactionDBTypetoTransactionType(
-      transactions,
+  const getTransactionListComponent = ([date, transactions]: [
+    string,
+    types.TransactionDBType[],
+  ]) => {
+    const transactionList = TransactionStore.isFiltered
+      ? filterList(transactions)
+      : transactions;
+    if (transactionList.length === 0) {
+      return <></>;
+    }
+    const formattedTransactionList = convertTransactionDBTypetoTransactionType(
+      transactionList,
     );
-    if (filteredTransactionList.length === 0) return '';
     return (
       <TransactionList
         key={date}
         date={new Date(date)}
         onClick={onClick}
-        transactionList={filteredTransactionList}
+        transactionList={formattedTransactionList}
       />
     );
   };
 
-  return <>{Object.entries(list).map(mapFunc)}</>;
+  return <>{Object.entries(list).map(getTransactionListComponent)}</>;
 };
 
 export default observer(TransactionDateList);
