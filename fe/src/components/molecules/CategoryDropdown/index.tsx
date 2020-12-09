ae05f2ce0bdf84d90841d788ed788aeceb707c48
rolled-potatoes <keep4404@gmail.com>
2020-12-10 00:25:15 +0900

    [FE] Feat : 거래수단 중 미분류 리스트안뜨게 수정

--- fe/src/components/molecules/CategoryDropdown/index.tsx
+++ fe/src/components/molecules/CategoryDropdown/index.tsx
@@ -22,38 +22,41 @@ const CategoryDropdown = ({
   return (
     <S.DropdownBodyWrap>
       {dataList.map(
-        (data: any): React.ReactElement => (
-          <S.DropdownItem key={data._id} {...props}>
-            <div className="flex-container modify-button">
-              <S.CategoryEditButton
-                key={`category-edit-${data._id}`}
-                isClicked={isClicked}
-                onClick={(e: MouseEvent) => {
-                  e.preventDefault();
-                  dropDownItemClicked({ ...data });
-                }}
-                type="button"
-                value="수정"
-              />
-            </div>
-            {!!data.color && <S.ColorBox color={data.color}> </S.ColorBox>}
-            <div className="title-container flex-container">
-              <span key={data.objectId}>{data.title}</span>
-            </div>
-            <div className="flex-container">
-              <S.CategoryEditButton
-                key={data._id}
-                isClicked={isClicked}
-                onClick={(e: MouseEvent) => {
-                  e.preventDefault();
-                  deleteClicked(data._id);
-                }}
-                type="button"
-                value="삭제"
-              />
-            </div>
-          </S.DropdownItem>
-        ),
+        (data: any): React.ReactElement => {
+          if (data.title === '미분류') return <></>;
+          return (
+            <S.DropdownItem key={data._id} {...props}>
+              <div className="flex-container modify-button">
+                <S.CategoryEditButton
+                  key={`category-edit-${data._id}`}
+                  isClicked={isClicked}
+                  onClick={(e: MouseEvent) => {
+                    e.preventDefault();
+                    dropDownItemClicked({ ...data });
+                  }}
+                  type="button"
+                  value="수정"
+                />
+              </div>
+              {!!data.color && <S.ColorBox color={data.color}> </S.ColorBox>}
+              <div className="title-container flex-container">
+                <span key={data.objectId}>{data.title}</span>
+              </div>
+              <div className="flex-container">
+                <S.CategoryEditButton
+                  key={data._id}
+                  isClicked={isClicked}
+                  onClick={(e: MouseEvent) => {
+                    e.preventDefault();
+                    deleteClicked(data._id);
+                  }}
+                  type="button"
+                  value="삭제"
+                />
+              </div>
+            </S.DropdownItem>
+          );
+        },
       )}
     </S.DropdownBodyWrap>
   );
