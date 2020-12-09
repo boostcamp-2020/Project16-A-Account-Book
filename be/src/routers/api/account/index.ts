c30290d4c16b829dd46bf620fa6ee7b4aea68ded
pkiop <pkiop@naver.com>
2020-12-09 22:50:01 +0900

    [BE] Feat: 가계부 생성위한 post라우팅

--- be/src/routers/api/account/index.ts
+++ be/src/routers/api/account/index.ts
@@ -5,6 +5,7 @@ const router = new Router();
 
 router.get('/info', accountController.getThisAccountInfo);
 router.get('/', accountController.get);
+router.post('/', accountController.postAccount);
 router.post('/user', accountController.postAccountUser);
 
 export default router;
