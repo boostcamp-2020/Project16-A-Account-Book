11961d9866449b6d2e882751160d243f9bd87552
dbstjrwnekd <sh4869@ajou.ac.kr>
2020-12-09 16:50:16 +0900

    Fix : 채팅 페이지
    - 채팅페이지 생성
    - api작업 필요

---
+++ fe/src/components/templates/ChattingTemplate/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ChattingContainer = styled.div``;
+
+export const ChattingHeader = styled.div``;
+
+export const ChattingContents = styled.div``;
+
+export const ChattingBottom = styled.div``;
