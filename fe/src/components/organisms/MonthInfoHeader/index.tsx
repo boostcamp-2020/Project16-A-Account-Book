85bbffa46b875591de1eda2f3d0be718b19cef6a
yejineee <yygeniee@gmail.com>
2020-12-09 16:14:20 +0900

    [FE] Feat: Month Info Header의 날짜를 스토어에서 가져오도록 변경
    MonthInfoHeader는 다양한 페이지에서 동일한 값으로 사용하게 된다.
    따라서 날짜와 총 금액을 optional props로 받고, 디폴트로는 스토어에서
    날짜와 가격을 가져오도록 하였다.
    스토어에 있는 날짜는 하루가 초과된 날짜이므로 하루를 뺀 값을 넣도록 하였다.

--- fe/src/components/organisms/MonthInfoHeader/index.tsx
+++ fe/src/components/organisms/MonthInfoHeader/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import TotalBox from 'components/molecules/TotalBox';
+import { observer } from 'mobx-react-lite';
+import { TransactionStore } from 'stores/Transaction';
+import dateUtils from 'utils/date';
 import { MonthInfoHeaderContainer, MonthButton } from './style';
 
 interface MonthInfoHeaderInterface {
-  month: number;
-  total: {
+  date?: {
+    startDate: string;
+    endDate: string;
+  };
+  total?: {
     income: number;
     expense: number;
   };
 }
 
-const MonthInfoHeader = ({ month, total }: MonthInfoHeaderInterface) => {
+const MonthInfoHeader = ({
+  date = {
+    startDate: TransactionStore.getDates().startDate,
+    endDate: dateUtils.subTractDate(TransactionStore.getDates().endDate),
+  },
+  total = TransactionStore.totalPrices,
+}: MonthInfoHeaderInterface) => {
   return (
     <MonthInfoHeaderContainer>
       <MonthButton onClick={() => {}} size="xl" border>
-        {`${month}월 ▾`}
+        <>
+          {`시작 날짜 : ${date.startDate}`}
+          {`끝 날짜 : ${date.endDate}`}
+        </>
       </MonthButton>
 
       <div>
@@ -29,4 +44,4 @@ const MonthInfoHeader = ({ month, total }: MonthInfoHeaderInterface) => {
   );
 };
 
-export default MonthInfoHeader;
+export default observer(MonthInfoHeader);
