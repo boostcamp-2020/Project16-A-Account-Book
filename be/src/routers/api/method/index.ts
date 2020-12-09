450d5e523a58848c770796ad3c5db32eb76fb83c
rolled-potatoes <keep4404@gmail.com>
2020-12-09 20:27:21 +0900

    [BE] Feat : 메소드 등록 API 작성

--- be/src/routers/api/method/index.ts
+++ be/src/routers/api/method/index.ts
@@ -2,5 +2,8 @@ import Router from 'koa-router';
 import methodController from 'controllers/method';
 
 const router = new Router();
+
 router.get('/', methodController.get);
+router.post('/', methodController.post);
+
 export default router;
