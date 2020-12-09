9abb16f6305f236a3289efcb41a1e406d53fac08
pkiop <pkiop@naver.com>
2020-12-09 21:26:38 +0900

    [FE] Feat: profileImgList 컴포넌트 생성 및 스토리 작성

---
+++ fe/src/components/molecules/ProfileImgList/style.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import IconComponent from 'components/atoms/MoveIcon';
+
+export const Icon = styled(IconComponent)`
+  border-radius: 2rem;
+  position: absolute;
+  right: ${({ rightMargin }: any) => {
+    return rightMargin;
+  }}rem;
+  top: 0;
+  transition: 0.5s;
+`;
+
+export const Wrap = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  :hover {
+    & > :nth-child(2) {
+      right: 2.5rem;
+    }
+    & > :nth-child(3) {
+      right: 5rem;
+    }
+    & > :nth-child(4) {
+      right: 7.5rem;
+    }
+    & > :nth-child(5) {
+      right: 10rem;
+    }
+  }
+`;
