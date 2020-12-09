2feb0a8aa487afac6abfc1048b0e6cb252dc91a7
pkiop <pkiop@naver.com>
2020-12-09 11:22:58 +0900

    [BE] Fix: getAccountsByUserId로 해당 accountList얻도록 수정

--- be/src/controllers/account/index.ts
+++ be/src/controllers/account/index.ts
@@ -2,9 +2,18 @@ import Koa from 'koa';
 import * as accountService from 'services/account';
 
 export const get = async (ctx: Koa.Context) => {
-  const res = await accountService.getAccounts(ctx.request.body.user.accounts);
+  const res = await accountService.getAccountsByUserId(
+    ctx.request.body.user._id,
+  );
+  console.log('res : ', res);
+
+  if (!res || !res.accounts) {
+    ctx.status = 204;
+    ctx.response.body = [];
+    return;
+  }
   ctx.status = 200;
-  ctx.response.body = res;
+  ctx.response.body = res.accounts;
 };
 
 export const getThisAccountInfo = async (ctx: Koa.Context) => {
