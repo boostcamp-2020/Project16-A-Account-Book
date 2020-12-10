9d8d98aa6f7a87e986dda3d54ec7fdd038bb504a
rolled-potatoes <keep4404@gmail.com>
2020-12-10 21:37:01 +0900

    [BE] Feat : 사용자 가계부 라우터 생성

---
+++ be/src/routers/api/user/account.ts
@@ -0,0 +1,6 @@
+import Router from 'koa-router';
+
+const router = new Router();
+
+router.get('/', () => {});
+export default router;
--- be/src/routers/api/user/index.ts
+++ be/src/routers/api/user/index.ts
@@ -1,8 +1,9 @@
 import Router from 'koa-router';
 import * as userController from 'controllers/user';
+import accountRouter from './account';
 
 const router = new Router();
 
 router.get('/titleByAccountId', userController.titleByAccountId);
-
+router.use('/accounts', accountRouter.routes());
 export default router;
