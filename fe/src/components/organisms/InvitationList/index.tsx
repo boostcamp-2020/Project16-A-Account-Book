05613be07086ebe3d00a6615b5c3fddb48f962a3
rolled-potatoes <keep4404@gmail.com>
2020-12-10 18:35:30 +0900

    [FE] Feat : 초대 리스트 컴포넌트 초안 생성

--- fe/src/components/organisms/InvitationList/index.tsx
+++ fe/src/components/organisms/InvitationList/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import InvitationItem from 'components/molecules/InvitationItem';
 import Container from './style';
 
-const InvitationList = () => {
-  return <Container>InvitationList</Container>;
+export interface Prop {
+  list: any[];
+}
+const InvitationList = ({ list }: Prop) => {
+  const invitations = list.map((l) => {
+    return <InvitationItem {...l} />;
+  });
+  return <Container>{invitations}</Container>;
 };
 
 export default InvitationList;
