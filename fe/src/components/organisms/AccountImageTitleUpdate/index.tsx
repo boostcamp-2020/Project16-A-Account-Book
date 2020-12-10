eed9abfcc616da94c801b2a8857c84fcf9e7626f
pkiop <pkiop@naver.com>
2020-12-10 14:54:05 +0900

    [FE] Feat: 컴포넌트 작성

---
+++ fe/src/components/organisms/AccountImageTitleUpdate/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as S from './style';
+
+export interface Props {
+  account: any;
+}
+
+const AccountImageTitleUpdate = ({ account, ...props }: Props) => {
+  const text = '가계부 이름';
+  return (
+    <S.AccountImageTitleUpdate {...props}>
+      <div className="imageWrap">
+        <S.Icon icon={account.icon} />
+        <S.UploadBtn>업로드</S.UploadBtn>
+      </div>
+      <div className="textWrap">
+        <div className="title">{text}</div>
+        <S.TitleInput placeholder="가계부 제목을 입력하세요" />
+      </div>
+    </S.AccountImageTitleUpdate>
+  );
+};
+
+export default AccountImageTitleUpdate;
