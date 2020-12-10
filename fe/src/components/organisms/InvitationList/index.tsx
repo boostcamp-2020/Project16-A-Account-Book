685bed2e3dc04f7a5cbbfc28d193327e310d62b4
rolled-potatoes <keep4404@gmail.com>
2020-12-10 18:30:06 +0900

    [FE] Feat : 초대 목록 파일 생성

---
+++ fe/src/components/organisms/InvitationList/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Container from './style';
+
+const InvitationList = () => {
+  return <Container>InvitationList</Container>;
+};
+
+export default InvitationList;
---
+++ fe/src/components/organisms/InvitationList/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Container = styled.section``;
+
+export default Container;
