6d7d03205647210aa15951e05ddc61df1d4f73a2
pkiop <pkiop@naver.com>
2020-12-10 23:00:00 +0900

    [FE] Feat: AuthCheck에서 userObjId없을 때 다시 넣어주는 로직 추가

--- fe/src/pages/AuthCheck/index.tsx
+++ fe/src/pages/AuthCheck/index.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react';
+import axios from 'apis/axios';
+import url from 'apis/urls';
 
 const AuthCheck = () => {
   useEffect(() => {});
+  if (!sessionStorage.getItem('userObjId')) {
+    axios.get(`${url.userInfo}`).then((res) => {
+      sessionStorage.setItem('userObjId', (res as any)._id);
+    });
+  }
   return <></>;
 };
 
