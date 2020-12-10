256dafc11ce17b70139724f2d086ce699a6cd1f4
pkiop <pkiop@naver.com>
2020-12-10 23:03:03 +0900

    [FE] Feat: userInfo 받는 url추가

--- fe/src/apis/urls/index.ts
+++ fe/src/apis/urls/index.ts
@@ -9,6 +9,7 @@ export default {
   github: '/api/auth/github',
   githubAccessToken: '/api/auth/github/access_token?code=',
   user: `/api/users`,
+  userInfo: `/api/users/userInfo`,
   userTitle: '/api/users/titleByAccountId?accountId=',
   category: (accountObjId: string) => {
     return `/api/${accountObjId}/categories/`;
