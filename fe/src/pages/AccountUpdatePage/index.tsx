720d0c4b40cea0972b4af3a4c595d7fa625d8383
yejineee <yygeniee@gmail.com>
2020-12-10 18:32:09 +0900

    [FE] Feat: 가계부 수정 페이지에서 사용자 추가를 위한 이벤트 핸들러 및 유저 리스트 전달
    가계부 수정 페이지에서 사용자 추가를 위해 현재
    유저 리스트와 체크된 유저 리스트를 InviteUser 컴포넌트에 전달한다.
    유저 컴포넌트 클릭 시, 체크를 하기 위한 onSelectUser 핸들러를 추가하였다.

--- fe/src/pages/AccountUpdatePage/index.tsx
+++ fe/src/pages/AccountUpdatePage/index.tsx
@@ -3,15 +3,44 @@ import { observer } from 'mobx-react-lite';
 import Template from 'components/templates/MainTemplate';
 import Header from 'components/organisms/HeaderBar';
 import AccountTitleImageUpdate from 'components/organisms/AccountImageTitleUpdate';
+import InviteUser from 'components/organisms/InviteUser';
+import useInviteUser from 'hooks/useInviteUser';
+import utils from 'utils';
+import { IUser } from 'types';
 
 interface Props {
   location?: any;
 }
 
 const AccountUpdatePage = ({ location }: Props) => {
+  const { account } = location.state;
+  const [userList, checkedUserIdList, setCheckedUserIdList] = useInviteUser(
+    account.users,
+  );
+
+  const onSelectUser = (user: IUser) => {
+    const selectedId = user._id;
+    if (selectedId === 'ALL') {
+      if (checkedUserIdList.length === userList.length) {
+        setCheckedUserIdList([]);
+      } else {
+        setCheckedUserIdList(userList.map((userData) => userData._id));
+      }
+    } else {
+      setCheckedUserIdList((prevCheckedList: string[]) =>
+        utils.toggleTargetInList<string>(prevCheckedList, selectedId),
+      );
+    }
+  };
+
   const Contents = (
     <>
-      <AccountTitleImageUpdate account={location.state.account} />
+      <AccountTitleImageUpdate account={account} />
+      <InviteUser
+        dataList={userList}
+        onClick={onSelectUser}
+        checkList={checkedUserIdList}
+      />
     </>
   );
 
