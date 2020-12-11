4240c020957184ded1537fa32564def9498b26fb
pkiop <pkiop@naver.com>
2020-12-11 10:34:56 +0900

    [BE] Fix: Context Koa에서 구조분해로 꺼내오기

--- be/src/controllers/user/index.ts
+++ be/src/controllers/user/index.ts
@@ -1,24 +1,24 @@
-import Koa from 'koa';
+import { Context } from 'koa';
 import * as userService from 'services/user';
 
-export const titleByAccountId = async (ctx: Koa.Context) => {
+export const titleByAccountId = async (ctx: Context) => {
   const { accountId } = ctx.query;
   const title = await userService.titleByAccountId(accountId);
   ctx.status = 200;
   ctx.response.body = title;
 };
 
-export const getUserList = async (ctx: Koa.Context) => {
+export const getUserList = async (ctx: Context) => {
   const userList = await userService.getUserList();
   ctx.status = 200;
   ctx.body = userList;
 };
 
-export const getUserByAccessToken = async (ctx: Koa.Context) => {
+export const getUserByAccessToken = async (ctx: Context) => {
   ctx.status = 200;
   ctx.body = ctx.request.body.user;
 };
-export const getInvitation = async (ctx: Koa.Context) => {
+export const getInvitation = async (ctx: Context) => {
   const { user } = ctx.request.body;
   const accounts = await userService.getInvitation(user);
   const invitations = accounts.map((account: any, idx) => ({
