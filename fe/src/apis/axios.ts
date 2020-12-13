db0912e8248fef62bc73e7bbcead0bb459603ca0
rolled-potatoes <keep4404@gmail.com>
2020-12-13 20:36:30 +0900

    [FE] Chore : axios base url 제거
    리액트에서의 Proxy를 사용하여 대신 할 수 있을 것으로 판단하여 제거.

--- fe/src/apis/axios.ts
+++ fe/src/apis/axios.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`,
 });
 
 instance.interceptors.response.use(
