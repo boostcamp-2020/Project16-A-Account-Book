1716651243c55ed73420800dd74211b497a1a614
rolled-potatoes <keep4404@gmail.com>
2020-12-14 22:54:23 +0900

    [FE] Style : 필터 드랍다운 높이 지정

--- fe/src/components/molecules/DropdownBody/style.ts
+++ fe/src/components/molecules/DropdownBody/style.ts
@@ -10,6 +10,8 @@ export const DropdownBodyWrap = styled.div`
   position: absolute;
   background-color: white;
   width: inherit;
+  max-height: 35vh;
+  overflow-y: auto;
   z-index: 10;
   border: 1px solid ${({ theme }) => theme.color.subText};
   padding: 0.3em;
@@ -26,5 +28,12 @@ export const DropdownBodyWrap = styled.div`
     background: transparent;
     display: flex;
     color: ${({ theme }) => theme.color.black};
+  }
+  @media only screen and (max-width: 600px) {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    max-width: 50vw;
+    transform: translate(-50%, -50%);
   }
 `;
