2e09adf2dd38d1251eda49fcd96ed125a3614172
pkiop <pkiop@naver.com>
2020-12-15 23:36:44 +0900

    [FE] Feat: 데이터 없을 때 보여줄 컴포넌트 작성

--- fe/src/components/organisms/NoData/index.tsx
+++ fe/src/components/organisms/NoData/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import * as S from './style';
 
 const Nodata = ({ ...props }): React.ReactElement => {
-  return <S.NoData {...props}>데이터가 없습니다.</S.NoData>;
+  return (
+    <S.NoData {...props}>
+      <div className="image" />
+      <div>해당기간 거래내역이 없습니다.</div>
+    </S.NoData>
+  );
 };
 
 export default Nodata;
