14bf28e19b2db652182390dd4152dd55c4af7def
pkiop <pkiop@naver.com>
2020-12-15 23:13:41 +0900

    [FE] Feat: 404페이지 석봉이미지와 텍스트 추가

--- fe/src/pages/NotFoundPage/index.tsx
+++ fe/src/pages/NotFoundPage/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import * as S from './style';
 
 const NotFoundPage = () => {
-  return <div>404page~</div>;
+  return (
+    <S.NotFound>
+      <img
+        src="https://camo.githubusercontent.com/9acad1537ebb0c3e10abbf4b74ef2d1929c2d504a91e8cd49bfcd1ddbeff4f9d/68747470733a2f2f692e696d6775722e636f6d2f633238335a764a2e706e67"
+        width="300px"
+        alt="404"
+      />
+      <div className="big">404</div>
+      <div className="small">페이지를 찾을 수 없습니다.</div>
+    </S.NotFound>
+  );
 };
 
 export default NotFoundPage;
