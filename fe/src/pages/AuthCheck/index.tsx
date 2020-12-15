d5793303948aa7060cb6d4cacec652c08cbfcd05
pkiop <pkiop@naver.com>
2020-12-15 21:20:21 +0900

    [FE] Fix: 불필요한 useEffect 제거

--- fe/src/pages/AuthCheck/index.tsx
+++ fe/src/pages/AuthCheck/index.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import axios from 'apis/axios';
 import url from 'apis/urls';
 
 const AuthCheck = () => {
-  useEffect(() => {});
   if (!sessionStorage.getItem('userObjId')) {
     axios.get(`${url.userInfo}`).then((res) => {
       sessionStorage.setItem('userObjId', (res as any)._id);
