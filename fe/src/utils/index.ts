03d1a3491a54c0d98ee07ccbb0883cef4d846378
yejineee <yygeniee@gmail.com>
2020-12-16 23:50:46 +0900

    [FE] Feat: 돈이 주어졌을 때, x.x만 처럼 단위로 변환해주는 유틸 함수 구현
    돈의 단위가 너무 커졌을 때 적절하게 값을 보여주기 위해서
    x.x만 처럼 단위를 한글로 변환해주는 유틸함수를 구현하였다.

--- fe/src/utils/index.ts
+++ fe/src/utils/index.ts
@@ -11,4 +11,33 @@ export default {
     }
     return [...set];
   },
+  summaryOfMoney(money: number): string {
+    const NUM_OF_DECIMAL_PLACE = 1;
+    const matcher = [
+      [10 ** 3, '천'],
+      [10 ** 4, '만'],
+      [10 ** 5, '십만'],
+      [10 ** 6, '백만'],
+      [10 ** 7, '천만'],
+      [10 ** 8, '억'],
+      [10 ** 9, '십억'],
+      [10 ** 10, '백억'],
+      [10 ** 11, '천억'],
+    ];
+    let matched;
+
+    type matcherType = [divisionBy: number | any, unit: string];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const matcherItem of matcher) {
+      const [divisionBy, unit] = matcherItem as matcherType;
+      if (Math.floor(money / divisionBy) === 0) {
+        break;
+      }
+      matched = [divisionBy, unit];
+    }
+    const [matchedDivisitonBy, matchedUnit] = matched;
+    return `${Number.parseFloat(`${money / matchedDivisitonBy}`).toFixed(
+      NUM_OF_DECIMAL_PLACE,
+    )}${matchedUnit}`;
+  },
 };
