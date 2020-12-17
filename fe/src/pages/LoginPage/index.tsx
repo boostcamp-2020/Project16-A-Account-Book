fdb638f90a7a22d7be45b39a5ede7bb3a2cbbf67
pkiop <pkiop@naver.com>
2020-12-17 23:29:38 +0900

    [FE] Feat: 타이틀, 이미지 추가

--- fe/src/pages/LoginPage/index.tsx
+++ fe/src/pages/LoginPage/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-// import { useHistory } from 'react-router-dom'
 import LoginTemplate from 'components/templates/LoginTemplate';
 import LoginButtons from 'components/organisms/LoginButtons';
-import Header from 'components/organisms/Header';
+import * as S from './style';
 
+const logoUrl =
+  'https://camo.githubusercontent.com/9acad1537ebb0c3e10abbf4b74ef2d1929c2d504a91e8cd49bfcd1ddbeff4f9d/68747470733a2f2f692e696d6775722e636f6d2f633238335a764a2e706e67';
 function LoginPage(): React.ReactElement {
-  return (
-    <LoginTemplate headerContent={<Header />} oauthContent={<LoginButtons />} />
+  const Contents = (
+    <S.Wrap>
+      <div className="mainTitle">N 석 봉</div>
+      <img src={logoUrl} alt="logo" />
+      <LoginButtons />
+    </S.Wrap>
   );
+  return <LoginTemplate oauthContent={Contents} />;
 }
 
 export default LoginPage;
