a987969ca3765b6c45119f4f192013c21cf890b0
pkiop <pkiop@naver.com>
2020-12-17 23:33:36 +0900

    [FE] Fix: 배경색 브랜드컬러로 깔기

--- fe/src/components/templates/LoginTemplate/style.ts
+++ fe/src/components/templates/LoginTemplate/style.ts
@@ -4,7 +4,9 @@ export const Container = styled.div`
   margin: 0;
   padding: 0;
   text-align: center;
+  background-color: ${({ theme }) => theme.color.brandColor};
   width: 100%;
+  height: 5000px;
 `;
 
 export const HeaderContainer = styled.div`
