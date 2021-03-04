f2a24867ce60c219fef23e3a983898a4070d26c1
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-03-04 13:15:29 +0900

    [BE] Refactor_User_Service
    - invitation관련코드 수정 필요

--- be/src/services/user/index.ts
+++ be/src/services/user/index.ts
@@ -1,68 +1,72 @@
-import { UserModel, IUserDocument } from 'models/user';
-import { AccountModel } from 'models/account';
+const models = require('models');
 
 export const titleByAccountId = async (accountId: String) => {
-  const account = await AccountModel.findOne(
-    { _id: accountId },
-    { _id: false, title: true },
-  ).exec();
+  const account = await models.Account.findOne({
+    attributes: ['title'],
+    where: {
+      id: accountId,
+    },
+  });
 
   return account;
 };
 
 export const getUserList = async () => {
-  const allUserList = await UserModel.find({}).exec();
+  const allUserList = await models.User.findAll({});
   return allUserList;
 };
 
-export const putUser = async (user: IUserDocument, startOfWeek: String) => {
-  const result = user.update({ startOfWeek });
+export const putUser = async (user: any, startOfWeek: String) => {
+  const result = models.User.update(
+    { startOfWeek },
+    { where: { id: user.id } },
+  );
   return result;
 };
 
-export const getInvitation = async (user: IUserDocument) => {
-  const account: any = user.invitations?.map((invitation) =>
-    AccountModel.findById(invitation.accounts).select(
-      'title _id ownerName imageUrl',
-    ),
-  );
-  const results = await Promise.all(account);
-  return results;
-};
+// export const getInvitation = async (user: IUserDocument) => {
+//   const account: any = user.invitations?.map((invitation) =>
+//     AccountModel.findById(invitation.accounts).select(
+//       'title _id ownerName imageUrl',
+//     ),
+//   );
+//   const results = await Promise.all(account);
+//   return results;
+// };
 
-const filterInvitation = (user: IUserDocument, accountObjId: string) => {
-  const prevInvitaionLength = user.invitations?.length;
-  // eslint-disable-next-line no-param-reassign
-  user.invitations = user.invitations?.filter(
-    (invitation) => String(invitation.accounts) !== accountObjId,
-  );
-  return user.invitations?.length === prevInvitaionLength
-    ? Promise.resolve()
-    : user.save();
-};
+// const filterInvitation = (user: IUserDocument, accountObjId: string) => {
+//   const prevInvitaionLength = user.invitations?.length;
+//   // eslint-disable-next-line no-param-reassign
+//   user.invitations = user.invitations?.filter(
+//     (invitation) => String(invitation.accounts) !== accountObjId,
+//   );
+//   return user.invitations?.length === prevInvitaionLength
+//     ? Promise.resolve()
+//     : user.save();
+// };
 
-export const denyInvitation = async (
-  user: IUserDocument,
-  accountObjId: string,
-) => {
-  return filterInvitation(user, accountObjId);
-};
+// export const denyInvitation = async (
+//   user: IUserDocument,
+//   accountObjId: string,
+// ) => {
+//   return filterInvitation(user, accountObjId);
+// };
 
-export const agreeInvitaion = async (
-  user: IUserDocument,
-  accountObjId: string,
-) => {
-  return Promise.all([
-    filterInvitation(user, accountObjId),
-    AccountModel.update(
-      { _id: accountObjId, 'users._id': { $ne: user._id } },
-      {
-        $addToSet: {
-          users: user,
-        },
-      },
-    ),
-  ]);
-};
+// export const agreeInvitaion = async (
+//   user: IUserDocument,
+//   accountObjId: string,
+// ) => {
+//   return Promise.all([
+//     filterInvitation(user, accountObjId),
+//     AccountModel.update(
+//       { _id: accountObjId, 'users._id': { $ne: user._id } },
+//       {
+//         $addToSet: {
+//           users: user,
+//         },
+//       },
+//     ),
+//   ]);
+// };
 
 export default titleByAccountId;
